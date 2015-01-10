d0247855cd24814f582a50b08d855532ca2aa166
mike <mike@mydebian.domain_not_set.invalid>
2015-01-10 12:59:06 -0500

    font for a-laptop-faster

--- init.el
+++ init.el
@@ -623,8 +623,16 @@ Resize-window = t will adjust the window so the modeline fits on screen, etc."
    ;; If you edit it by hand, you could mess it up, so be careful.
    ;; Your init file should contain only one such instance.
    ;; If there is more than one, they won't work right.
-   '(default ((t (:family "Fixed" :foundry "Misc" :slant normal :weight normal :height 150 :width normal)))))
-  )
+   '(default ((t (:family "Fixed" :foundry "Misc" :slant normal :weight normal :height 150 :width normal))))))
+
+(when (eq my/curr-computer 'a-laptop-faster)
+  (custom-set-faces
+   '(default ((t (:family "Inconsolata"
+                          :foundry "unknown"
+                          :slant normal
+                          :weight normal
+                          :height 128
+                          :width normal))))))
 
 ;;----------------------------------
 ;; cursor

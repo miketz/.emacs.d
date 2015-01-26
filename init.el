c575b5e94e6a1ec8b2949eac7bee69f895241d80
unknown <miketz@users.noreply.github.com>
2015-01-26 17:59:37 -0500

    paren colors

--- init.el
+++ init.el
@@ -892,6 +892,22 @@ This prevents overlapping themes; something I would rarely want."
    '(rainbow-delimiters-depth-9-face ((t (:foreground "orange"))))
    '(rainbow-delimiters-unmatched-face ((t (:foreground "black" :background "red"))))))
 
+(defun my/rainbow-parens-light-bg2 ()
+  "Colored parens with highlighting."
+  (interactive)
+  (custom-set-faces
+   '(rainbow-delimiters-depth-1-face ((t (:foreground "black"))))
+   '(rainbow-delimiters-depth-2-face ((t (:foreground "black" :background "light cyan"))))
+   '(rainbow-delimiters-depth-3-face ((t (:foreground "red" :background "#faEaEa"))))
+   '(rainbow-delimiters-depth-4-face ((t (:foreground "purple" :background "lavenderblush"))))
+   '(rainbow-delimiters-depth-5-face ((t (:foreground "black" :background "lemon chiffon"))))
+   '(rainbow-delimiters-depth-6-face ((t (:foreground "magenta" :background "#EEEEFF"))))
+   '(rainbow-delimiters-depth-7-face ((t (:foreground "gray52"))))
+   '(rainbow-delimiters-depth-8-face ((t (:foreground "indianred3"))))
+   '(rainbow-delimiters-depth-9-face ((t (:foreground "orange" :background "#fff7ca"))))
+   '(rainbow-delimiters-unmatched-face ((t (:foreground "yellow" :background "black"))))))
+
+
 (defun color-grandshell ()
   "Grandshell with a few mode-specific additoins."
   (interactive)

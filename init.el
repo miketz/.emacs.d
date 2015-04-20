c488ec98c2ee0af663b92f410362f2f3e69e679a
unknown <miketz@users.noreply.github.com>
2015-04-20 14:21:36 -0400

    integrate narrowing and indirect buffes

--- init.el
+++ init.el
@@ -3117,6 +3117,72 @@ Depends on evil mode."
 ;;     (insert (format "%s" m))
 ;;     (insert "\n")))
 
+
+;;------------------------------------------------------------------------------
+;; Integrate narrow-to-region with indirect buffers. To allow multiple
+;; major modes operatiing on 1 file.
+;;------------------------------------------------------------------------------
+(defun my/narrow-to-region-indirect (start end)
+  "Restrict editing in this buffer to the current region, indirectly."
+  (interactive "r")
+  (deactivate-mark)
+  (let ((buf-clone (clone-indirect-buffer nil nil))
+        (mode      (intern (completing-read
+                            "Mode: "
+                            (mapcar (lambda (e)
+                                      (list (symbol-name e)))
+                                    (apropos-internal "-mode$" 'commandp))
+                            nil t))))
+    (with-current-buffer buf-clone
+      (narrow-to-region start end)
+      (funcall mode))
+    (switch-to-buffer buf-clone)
+    ;;refresh syntax highlighting by toggling `font-lock-mode'
+    (dotimes (i 2)
+      (font-lock-mode))))
+
+;; (defvar indirect-mode-name nil
+;;   "Mode to set for indirect buffers.")
+;; (make-variable-buffer-local 'indirect-mode-name)
+;; (defun indirect-region (start end)
+;;   "Edit the current region in another buffer.
+;;     If the buffer-local variable `indirect-mode-name' is not set, prompt
+;;     for mode name to choose for the indirect buffer interactively.
+;;     Otherwise, use the value of said variable as argument to a funcall."
+;;   (interactive "r")
+;;   (let ((buffer-name (generate-new-buffer-name "*indirect*"))
+;;         (mode
+;;          (if (not indirect-mode-name)
+;;              (setq indirect-mode-name
+;;                    (intern
+;;                     (completing-read
+;;                      "Mode: "
+;;                      (mapcar (lambda (e)
+;;                                (list (symbol-name e)))
+;;                              (apropos-internal "-mode$" 'commandp))
+;;                      nil t)))
+;;            indirect-mode-name)))
+;;     (pop-to-buffer (make-indirect-buffer (current-buffer) buffer-name))
+;;     (funcall mode)
+;;     (narrow-to-region start end)
+;;     (goto-char (point-min))
+;;     (shrink-window-if-larger-than-buffer)))
+
+
+;; ;; `narrow-to-region-indirect' from Zane's blog:
+;; ;;    http://demonastery.org/2013/04/emacs-narrow-to-region-indirect/
+;; ;; NOTE: toggle `font-lock-mode' twice to clear highlighting.
+;; (defun narrow-to-region-indirect (start end)
+;;   "Restrict editing in this buffer to the current region, indirectly."
+;;   (interactive "r")
+;;   (deactivate-mark)
+;;   (let ((buf (clone-indirect-buffer nil nil)))
+;;     (with-current-buffer buf
+;;       (narrow-to-region start end))
+;;     (switch-to-buffer buf)))
+
+
+
 ;;------------------------------------------------------------------------------
 ;; Misc options. Keep this at the bottom
 ;;------------------------------------------------------------------------------

483da40e9163720947ce45d0cd61f7830111853b
unknown <miketz@users.noreply.github.com>
2015-05-07 12:01:36 -0400

    disabling ido/smex

--- init.el
+++ init.el
@@ -1940,17 +1940,17 @@ This prevents overlapping themes; something I would rarely want."
 ;;--------------------
 ;; Ido mode
 ;;--------------------
-;;(require 'ido)
-(setq ido-everywhere t)
-(ido-mode t)
+;; ;;(require 'ido)
+;; (setq ido-everywhere t)
+;; (ido-mode t)
 
 ;;----------------------
 ;; smex. (built on ido)
 ;;----------------------
-;; (smex-initialize) ; Can be omitted. This might cause a (minimal) delay
-;;                   ; when Smex is auto-initialized on its first run.
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; ;; (smex-initialize) ; Can be omitted. This might cause a (minimal) delay
+;; ;;                   ; when Smex is auto-initialized on its first run.
+;; (global-set-key (kbd "M-x") 'smex)
+;; (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 
 ;;--------------------
 ;; Yasnippet

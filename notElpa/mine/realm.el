d779f76ab60c51384a60e4795877c18ea2196623
mike <mike@Mike-PC.domain_not_set.invalid>
2015-05-14 22:19:47 -0400

    realm, a little more imp

--- notElpa/mine/realm.el
+++ notElpa/mine/realm.el
@@ -57,10 +57,55 @@ MODE => to turn on after narrowing."
                              key-bind-focus
                              key-bind-unfocus)
   "Doc."
-  `(with-eval-after-load (symbol-name ,:base-mode)
-     (require ,focus-mode) ;load it so we can bind keys.
+  `(with-eval-after-load (symbol-name ,base-mode)
+     ;;load mode library so we can bind keys.
+     (require ,focus-mode)
+     ;;using `cl-defun' to allow `return-from'
+     (cl-defun ,(conat "realm-" (symbol-name name) "-focus") ()
+       (interactive)
+       (let ((start-tag-name ,tag-start)
+             (end-tag-name   ,tag-end)
+             (start          nil)
+             (end            nil))
+         ;; Find start tag. Search backwards first to give priority to tag pairs
+         ;; the cursor is currently inside.
+         (setq start (search-backward start-tag-name nil t))
+         (when (null start)
+           ;; if start tag not found backwards, then try forwards.
+           (setq start (search-forward start-tag-name nil t)))
+         (when (null start)
+           (message "start tag not found")
+           (return-from my/focus-javascript nil))
+         ;;start is found, move cursor down a line, start highlighitng
+         (next-line)
+         (move-beginning-of-line nil)
+         (set-mark-command nil) ;(evil-visual-line)
+         ;; jump to end tag. always search forward
+         (setq end (search-forward end-tag-name nil t))
+         (when (null end)
+           (deactivate-mark)
+           (message "end tag not found")
+           (return-from my/focus-javascript nil))
+         ;;end tag is found. now move cursor up one line
+         (previous-line)
+         (move-end-of-line nil)
+         ;; turn on focus-mode for this region. (and narrow)
+         (call-interactively #'realm-focus focus-mode)))
 
-     (defun realm-)
+
+     (defun my/unfocus-javascript ()
+       "Undo the effects of `my/focus-javascript'."
+       (interactive)
+       (widen)
+       (web-mode))
+
+     ;; key bindings
+     (define-key web-mode-map (kbd "C-c j") #'my/focus-javascript)
+     ;; TODO: Use a different technique for this keybind. If we didn't enter
+     ;; `js2-mode' from `web-mode' then we don't want `my/unfocus-javascript' to
+     ;; turn on web-mode.
+     (define-key js2-mode-map (kbd "C-c u") #'my/unfocus-javascript)
+
      ))
 
 

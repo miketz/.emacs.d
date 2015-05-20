100274ce022f46fdbceaba89c6642d82299c2d98
unknown <miketz@users.noreply.github.com>
2015-05-20 16:06:50 -0400

    use lispy with slime

--- init.el
+++ init.el
@@ -1593,6 +1593,8 @@ This prevents overlapping themes; something I would rarely want."
                   (unless (slime-connected-p)
                     (save-excursion (slime)))))))
 
+  (add-hook 'slime-mode-hook #'lispy-mode)
+  (add-hook 'slime-repl-mode-hook #'lispy-mode)
   (add-hook 'slime-repl-mode-hook
             (lambda ()
               ;;turn off line numbers in the repl

c9771b5277eaeebb24e0f62f9d908c5eea96deb6
unknown <miketz@users.noreply.github.com>
2015-07-27 12:31:04 -0400

    rand distribution test

--- notElpa/mine/my-rand.el
+++ notElpa/mine/my-rand.el
@@ -13,3 +13,28 @@
 ;;;###autoload
 (defun rollCheck (val)
   (>= val (roll)))
+
+;; testing distribution of `rand'
+;; (let* ((times 1000000)
+;;        (rVals (loop for i from 1 to times
+;;                     collect (rand 1 100)))
+;;        (sum (apply #'+ rVals))
+;;        (len (length rVals))
+;;        (avg (/ sum len))
+;;        (counts (loop for n from 0 to 99
+;;                      collect 0)))
+;;   ;; count the distribution
+;;   (dolist (r rVals)
+;;     (setf (elt counts (- r 1))
+;;           (+ 1 (elt counts (- r 1)))))
+;;   ;; print
+;;   (insert (concat "avg: "
+;;                   (int-to-string avg)
+;;                   "\n"))
+;;   (insert "target: " (int-to-string (/ times 100)))
+;;   (insert "\n")
+;;   (dotimes (x 100)
+;;     (insert (int-to-string (+ x 1)))
+;;     (insert ": ")
+;;     (insert (int-to-string (elt counts x)))
+;;     (insert "\n\n")))

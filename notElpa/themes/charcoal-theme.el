32ab298f95e3d649a42918dc3f865f692e98fca4
unknown <miketz@users.noreply.github.com>
2015-07-30 17:13:29 -0400

    color change

--- notElpa/themes/charcoal-theme.el
+++ notElpa/themes/charcoal-theme.el
@@ -11,9 +11,11 @@
 
 ;;background: #35352B #000000
 (let ((charcoal-bg "#35352B")
-      (charcoal-fg "lemon chiffon"))
+      (charcoal-fg "gray80" ;"lightyellow3"
+       ))
   (custom-theme-set-faces
    'charcoal
    `(default ((t (:foreground ,charcoal-fg :background ,charcoal-bg))))))
+
 
 

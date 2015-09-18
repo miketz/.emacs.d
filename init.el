90b461d08c51de36ca29ffbf3bc2afd0c3af7ade
unknown <miketz@users.noreply.github.com>
2015-09-18 14:22:52 -0400

    more counsel bindings.
    NOTE: there seems to be a magit bug where things alreayd checked in are
    appearing again.

--- init.el
+++ init.el
@@ -2741,9 +2741,13 @@ To make it human readable."
     (define-key evil-normal-state-map (kbd "s") #'swiper)
     (evil-leader/set-key "b" #'ivy-switch-buffer))
 
-  (global-set-key (kbd "M-x") #'counsel-M-x)
-  (global-set-key (kbd "C-x C-f") #'counsel-find-file)
-  (global-set-key (kbd "<f9>") #'counsel-load-theme)
+  (progn ;; counsel completion augmentation
+    (global-set-key (kbd "M-x") #'counsel-M-x)
+    (global-set-key (kbd "C-x C-f") #'counsel-find-file)
+    ;; TODO: disable warning like i did for the other f9 binding for colors
+    (global-set-key (kbd "<f9>") #'counsel-load-theme)
+    (global-set-key (kbd "C-h v") #'counsel-describe-variable)
+    (global-set-key (kbd "C-h f") #'counsel-describe-function))
 
   (ivy-mode 1) ; turn on ivy completion
   )

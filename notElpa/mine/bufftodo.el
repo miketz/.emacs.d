a72faef11c7b35ca09827c5bfb6a576a03289fc4
unknown <miketz@users.noreply.github.com>
2015-09-24 10:16:38 -0400

    completed initial imp of bufftodo

--- notElpa/mine/bufftodo.el
+++ notElpa/mine/bufftodo.el
@@ -1,5 +1,8 @@
 (defvar bufftodo-lst '()
-  "The list of todo buffers.")
+  "The list of todo buffers.
+NOTE:
+`ibuffer' apperas to automatically modify this list if we delete a buffer.
+So we don't need to worry about adding functionality to ibuffer to keep this list in sync.")
 
 (defun bufftodo-clear-all ()
   "Clear buffers from the todo list."
@@ -11,17 +14,24 @@
   (interactive)
   (add-to-list 'bufftodo-lst buff nil #'eq))
 
+(defun bufftodo--read (lst)
+  "Return a manually selected buffer"
+  (get-buffer
+   (completing-read "Buf: "
+                    (mapcar (lambda (b)
+                              (buffer-name b))
+                            lst)
+                    nil t)))
+
 (defun bufftodo-add-selected-buff ()
   "Add a manually selected buffer to the todo list."
   (interactive)
-  (let ((buff (get-buffer
-               (completing-read "Buf: "
-                                (mapcar (lambda (b)
-                                          (buffer-name b))
-                                        (buffer-list))
-                                nil
-                                t))))
-    (bufftodo-add buff)))
+  (bufftodo-add (bufftodo--read (buffer-list))))
+
+(defun bufftodo-remove-selected-buff ()
+  "Manually select buffer in `bufftodo-lst', then remove it."
+  (interactive)
+  (setq bufftodo-lst (delq (bufftodo--read bufftodo-lst) bufftodo-lst)))
 
 (defun bufftodo-add-current-buff ()
   "Add the current buffer to the todo list."
@@ -44,11 +54,19 @@
   (let ((ibuffer-filtering-alist '((todo-only "todo"
                                               (lambda (buf qualifier)
                                                 (member buf bufftodo-lst))))))
-    (ibuffer t "TODO buffers" ibuffer-filtering-alist)))
+    (ibuffer nil "TODO buffers" ibuffer-filtering-alist)))
+
+;; keybinds. the "user interface"
+(global-set-key (kbd "C-c v") #'bufftodo-view)
+(global-set-key (kbd "C-c c") #'bufftodo-add-current-buff)
+(global-set-key (kbd "C-c a") #'bufftodo-add-selected-buff)
+(global-set-key (kbd "C-c w") #'bufftodo-clear-all) ; w -> "wipe"
+(global-set-key (kbd "C-c e") #'bufftodo-remove-selected-buff)
 
 (when nil ;; ad-hoc interactive testing
   (bufftodo-add-current-buff)
   (bufftodo-add-selected-buff)
+  (bufftodo-remove-selected-buff)
   (bufftodo-clear-all)
   (bufftodo-view)
   bufftodo-lst

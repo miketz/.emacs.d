a4b0c10972b21dceda6b4d2743dccb5a47c6b72c
mike <miketz@users.noreply.github.com>
2015-11-19 09:19:55 -0500

    autoload for my-color-majapahit

--- init.el
+++ init.el
@@ -876,6 +876,7 @@ This prevents overlapping themes; something I would rarely want."
 (autoload #'my-color-dichromacy "my-color-dichromacy" nil t)
 (autoload #'my-color-firebelly "my-color-firebelly" nil t)
 (autoload #'my-color-molokai "my-color-molokai" nil t)
+(autoload #'my-color-majapahit "my-color-majapahit" nil t)
 
 (when my-graphic-p ;; transparency stuff
   ;; TODO: auto load the transparency stuff

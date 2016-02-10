ba25d10bd7bf0ade7290e23550605914f184c0d3
mike <miketz@users.noreply.github.com>
2016-02-10 13:52:12 -0500

    refactor. Indent code after put into buffer.

--- init.el
+++ init.el
@@ -3209,46 +3209,51 @@ Region defined by START and END is automaticallyl detected by (interactive \"r\"
 
 ;;;-----------------------------------------------------------------------------
 ;;; Create a new buffer, stuff text in it, turn on mode.
+;;; TODO: move this into a new file. Add autoloads.
 ;;;-----------------------------------------------------------------------------
-(when nil ;; just a sample for now
-  ;; TODO: move this into a new file. Add autoloads.
+(defvar my-counter 0
+  "Sequential counter used for various things.")
 
-  (defvar my-counter 0
-    "Sequential counter used for various things.")
+(defun my-gen-buffer-name ()
+  (prog1
+      (concat "my-tmp-buf-"
+              (int-to-string my-counter))
+    (cl-incf my-counter)))
 
-  (defun my-gen-buffer-name ()
-    (prog1
-        (concat "my-tmp-buf-"
-                (int-to-string my-counter))
-      (cl-incf my-counter)))
+(defun my-kill-tmp-buffers ()
+  (interactive)
+  (dolist (b (buffer-list))
+    ;; TODO: implement
+    ))
 
-  (defun my-kill-tmp-buffers ()
-    (interactive)
-    (dolist (b (buffer-list))
-      ;; TODO: implement
-      ))
+(defun my-mode-on-region (start end)
+  "Switch to a new buffer with the highlighted text. Turn on a mode."
+  (interactive "r")
+  (my--mode-on-region start
+                      end
+                      (intern (completing-read
+                               "Mode: "
+                               (mapcar (lambda (e)
+                                         (list (symbol-name e)))
+                                       (apropos-internal "-mode$" #'commandp))
+                               nil t))))
 
-  (defun my-mode-on-region (start end)
-    "Switch to a new buffer with the highlighted text. Turn on a mode."
-    (interactive "r")
-    (my--mode-on-region start end (intern (completing-read
-                                           "Mode: "
-                                           (mapcar (lambda (e)
-                                                     (list (symbol-name e)))
-                                                   (apropos-internal "-mode$" #'commandp))
-                                           nil t))))
+(defun my-emacs-lisp-mode-on-region (start end)
+  (interactive "r")
+  (my--mode-on-region start end #'emacs-lisp-mode))
 
-  (defun my-emacs-lisp-mode-on-region (start end)
-    (interactive "r")
-    (my--mode-on-region start end #'emacs-lisp-mode))
+(defun my--mode-on-region (start end mode-fn)
+  (kill-ring-save start end) ;; copy higlihgted text
+  (deactivate-mark)
 
-  (defun my--mode-on-region (start end mode-fn)
-    (kill-ring-save start end) ;; copy higlihgted text
-    (deactivate-mark)
-    (switch-to-buffer-other-window (my-gen-buffer-name))
-    (yank) ;; paste text
-    ;; interactively select mode, turn it on.
-    (funcall mode-fn)))
+  (switch-to-buffer-other-window (my-gen-buffer-name))
+  (yank)              ;; paste text
+  (funcall mode-fn)   ;; interactively select mode, turn it on.
+  (mark-whole-buffer) ;; for auto indenting
+  (call-interactively #'indent-region) ;; interactive gets region autmoatically
+  )
+
+;; TODO: keybinds
 
 ;;;-----------------------------------------------------------------------------
 ;;; Focus javascript

9cbed6896321f1afc70277b60e95e23ad7577118
mike <miketz@users.noreply.github.com>
2016-02-24 16:22:02 -0500

    commented consolas font for reference

--- init.el
+++ init.el
@@ -970,6 +970,7 @@ This prevents overlapping themes; something I would rarely want."
    ((eq my-curr-computer 'work-laptop)
     ;; (set-frame-font "-raster-r_ansi-normal-normal-normal-mono-15-*-*-*-c-*-iso8859-1")
     ;; (set-frame-font "-raster-Dina-normal-normal-normal-mono-16-*-*-*-c-*-iso8859-1")
+    ;; (set-frame-font "-outline-Consolas-normal-normal-normal-mono-15-*-*-*-c-*-iso8859-1")
     (set-frame-font "-raster-peep-normal-normal-normal-mono-16-*-*-*-c-*-ms-oemlatin"))
 
    ((or (eq my-curr-computer 'leyna-laptop)

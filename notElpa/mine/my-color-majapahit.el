95476d0069d77b7a3e104a7b62c16ff1080bb05f
mike <miketz@users.noreply.github.com>
2016-02-25 13:18:38 -0500

    rainbow-delim faces for majapahit-theme

--- notElpa/mine/my-color-majapahit.el
+++ notElpa/mine/my-color-majapahit.el
@@ -63,7 +63,7 @@
 
 
    `(region
-     ((t (:background "#69685E"))));"#49483E"
+     ((t (:background "#69685E"))))     ;"#49483E"
 
    `(num3-face-even
      ((t (:underline nil
@@ -72,11 +72,30 @@
                      :background "black"
                      :foreground "yellow green"
                      :bold nil))))
-   '(leerzeichen ((t (:foreground "yellow4";"#A8A800"
-                                  :background "black";"#D4D4C8"
+   '(leerzeichen ((t (:foreground "yellow4" ;"#A8A800"
+                                  :background "black" ;"#D4D4C8"
                                   :italic nil
                                   :bold nil))))
    `(show-paren-match ((t (:slant italic
                                   :bold t
                                   :strike-through t
-                                  :background nil))))))
+                                  :background nil))))
+   '(rainbow-delimiters-depth-1-face ((t (:foreground "orange red"))))
+   '(rainbow-delimiters-depth-2-face ((t (:foreground "cyan"))))
+   '(rainbow-delimiters-depth-3-face ((t (:foreground "yellow"))))
+   '(rainbow-delimiters-depth-4-face ((t (:foreground "plum"))))
+   '(rainbow-delimiters-depth-5-face ((t (:foreground "lawn green"))))
+   '(rainbow-delimiters-depth-6-face ((t (:foreground "orange"))))
+   '(rainbow-delimiters-depth-7-face ((t (:foreground "white"))))
+   '(rainbow-delimiters-depth-8-face ((t (:foreground "hot pink" :background "#2F2F2F"))))
+   '(rainbow-delimiters-depth-9-face ((t (:foreground "burlywood3"))))
+   '(rainbow-delimiters-unmatched-face ((t (:foreground "sienna" :background "black")))))
+
+  ;; remove the bold from rainbow-delimiters.
+  ;; (dolist (f (face-list))
+  ;;   (when (my-str-starts-with-p (symbol-name f)
+  ;;                               "rainbow-delimiters-")
+  ;;     (set-face-attribute f
+  ;;                         nil ;; all frames
+  ;;                         :weight 'normal)))
+  )

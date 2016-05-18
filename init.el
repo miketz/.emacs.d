f0a34202dee49e60808326572bafb5728ecdaf58
mike <miketz@users.noreply.github.com>
2016-05-18 14:33:41 -0400

    unfinshed imp. Delay package code.

--- init.el
+++ init.el
@@ -178,6 +178,51 @@ Specific configs may be made based on the computer.")
 ;;;----------------------------------
 ;;; globals
 ;;;----------------------------------
+(defconst my-fast-load-p t
+  "When t, delay the use of packages.
+For when I want a very fast start without `package-initialize'. So emacs can be
+used for quick vim-like edits.
+Call fn `my-do-full-init-with-packages' to continue with the full load.")
+
+(defvar my--delayed-package-code '())
+
+;; TODO: complete this macro, test it.
+(cl-defmacro my-top-level-package-code (&rest body)
+  "Wraps code related to packages.
+This code would have been normal top-level code in the init.el file.
+It should *not* include code that would live inside `eval-after-load'.
+Wrapping in a macro allows optinally delaying the code to avoid a slow call
+to `package-initialize' during start up.
+The code is added to a list which can be invoked in the future with fn
+`my-do-full-init-with-packages'"
+  (declare (indent 0)) ;; indent like progn
+  (if my-fast-load-p
+      ;; don't expand/execute the code. Just add it to a list.
+      (loop for form in body ;; looping to sort of "splice" `body'
+            do
+            (add-to-list 'my--delayed-package-code form t))
+    ;; else, expand to normal code for immediate execution.
+    (let ((code (first body)))
+      `(print ,code))
+    ;; (let ((code '()))
+    ;;   (loop for form in body
+    ;;         do
+    ;;         (add-to-list 'code form t))
+    ;;   '(print "test"))
+    ))
+
+(defun my-do-full-init-with-packages ()
+  (interactive)
+  (package-initialize)
+  ;; TODO: Complete this, test it.
+  ;; run the code I would have normally my-delayed-package-codedone at start up
+  ;; for packages.
+  (cl-loop for form in my--delayed-package-code
+           do
+           (eval form)
+           ;(eval `(lambda () ,form) lexical-binding)
+           ))
+
 (defvar my-indent-width 4
   "An omni-variable serving 3 related purposes.
 Becuase I want them to have same value.

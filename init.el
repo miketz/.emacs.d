154284115631793e719e7f56bd1dfee5d6d8ccba
mike <miketz@users.noreply.github.com>
2016-06-09 16:00:05 -0400

    kosmos theme mods

--- init.el
+++ init.el
@@ -1033,6 +1033,7 @@ This prevents overlapping themes; something I would rarely want."
 (autoload #'my-color-molokai "my-color-molokai" nil t)
 (autoload #'my-color-majapahit "my-color-majapahit" nil t)
 (autoload #'my-color-deeper-blue "my-color-deeper-blue" nil t)
+(autoload #'my-color-kosmos "my-color-kosmos" nil t)
 
 (when my-graphic-p ;; transparency stuff
   ;; TODO: auto load the transparency stuff

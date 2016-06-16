bb27e00d582f67f958d5d4015275445066e22200
mike <miketz@users.noreply.github.com>
2016-06-16 15:00:12 -0400

    80 col rule

--- init.el
+++ init.el
@@ -2790,8 +2790,8 @@ and indent."
 ;; (eval-after-load 'company
 ;;   '(add-to-list 'company-backends 'company-irony))
 
-;; ;; (optional) adds CC special commands to `company-begin-commands' in order to
-;; ;; trigger completion at interesting places, such as after scope operator
+;; ;; (optional) adds CC special commands to `company-begin-commands' in order
+;; ;; to trigger completion at interesting places, such as after scope operator
 ;; ;;     std::|
 ;; (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
 

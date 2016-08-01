65b0f25dceeeab443cd53b01b8a0e381e8d042ed
mike <miketz@users.noreply.github.com>
2016-08-01 08:49:55 -0400

    bigger font

--- init.el
+++ init.el
@@ -1091,10 +1091,10 @@ This prevents overlapping themes; something I would rarely want."
          my-graphic-p)
     ;; (set-frame-font
     ;;  "-raster-Terminal-normal-normal-normal-mono-18-*-*-*-c-*-ms-oemlatin")
-    (set-frame-font
-     "-raster-r_ansi-normal-normal-normal-mono-15-*-*-*-c-*-iso8859-1")
     ;; (set-frame-font
-    ;;  "-raster-r_ansi-normal-normal-normal-mono-17-*-*-*-c-*-iso8859-1")
+    ;;  "-raster-r_ansi-normal-normal-normal-mono-15-*-*-*-c-*-iso8859-1")
+    (set-frame-font
+     "-raster-r_ansi-normal-normal-normal-mono-17-*-*-*-c-*-iso8859-1")
     ;; (set-frame-font
     ;;  "-raster-Fixedsys-normal-normal-normal-mono-17-*-*-*-c-*-iso8859-1")
     ;; (set-frame-font

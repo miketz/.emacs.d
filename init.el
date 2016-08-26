94ad184ae8d32b14c4fc5c744a8addd48a169ebc
mike <miketz@users.noreply.github.com>
2016-08-26 14:06:37 -0400

    comment overlay for emacs-lisp. cider slow to load

--- init.el
+++ init.el
@@ -4652,32 +4652,32 @@ When ARG isn't nil, try to pretty print the sexp."
 ;;; copied from:
 ;;; http://endlessparentheses.com/eval-result-overlays-in-emacs-lisp.html
 ;;;-----------------------------------------------------------------------------
-(autoload 'cider--make-result-overlay "cider-overlays")
+;; (autoload 'cider--make-result-overlay "cider-overlays")
 
-(defun endless/eval-overlay (value point)
-  (cider--make-result-overlay (format "%S" value)
-    :where point
-    :duration 'command)
-  ;; Preserve the return value.
-  value)
+;; (defun endless/eval-overlay (value point)
+;;   (cider--make-result-overlay (format "%S" value)
+;;     :where point
+;;     :duration 'command)
+;;   ;; Preserve the return value.
+;;   value)
 
-(advice-add 'eval-region :around
-            (lambda (f beg end &rest r)
-              (endless/eval-overlay
-               (apply f beg end r)
-               end)))
+;; (advice-add 'eval-region :around
+;;             (lambda (f beg end &rest r)
+;;               (endless/eval-overlay
+;;                (apply f beg end r)
+;;                end)))
 
-(advice-add 'eval-last-sexp :filter-return
-            (lambda (r)
-              (endless/eval-overlay r (point))))
+;; (advice-add 'eval-last-sexp :filter-return
+;;             (lambda (r)
+;;               (endless/eval-overlay r (point))))
 
-(advice-add 'eval-defun :filter-return
-            (lambda (r)
-              (endless/eval-overlay
-               r
-               (save-excursion
-                 (end-of-defun)
-                 (point)))))
+;; (advice-add 'eval-defun :filter-return
+;;             (lambda (r)
+;;               (endless/eval-overlay
+;;                r
+;;                (save-excursion
+;;                  (end-of-defun)
+;;                  (point)))))
 
 ;;;-----------------------------------------------------------------------------
 ;;; custom-file.

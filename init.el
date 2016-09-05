f77aba6b4c1a2f0a03862b1161ba43bd9bc414de
Mike <miketz@users.noreply.github.com>
2016-09-04 22:13:10 -0400

    ubuntu font on wild-dog

--- init.el
+++ init.el
@@ -1085,10 +1085,13 @@ This prevents overlapping themes; something I would rarely want."
 
   (cond
    ((eq my-curr-computer 'wild-dog)
+    (set-frame-font
+     (concat "-DAMA-Ubuntu Mono-normal-normal-normal-*-20-*-*-*-m-0-"
+             "iso10646-1"))
     ;; (set-frame-font
     ;;  "-xos4-Terminus-normal-normal-normal-*-22-*-*-*-c-110-iso10646-1")
-    (set-frame-font
-     "-xos4-Terminus-bold-normal-normal-*-22-*-*-*-c-110-iso10646-1")
+    ;; (set-frame-font
+    ;;  "-xos4-Terminus-bold-normal-normal-*-22-*-*-*-c-110-iso10646-1")
     ;; (set-frame-font
     ;;  "-xos4-Terminus-normal-normal-normal-*-18-*-*-*-c-100-iso10646-1")
     ;; (set-frame-font

41b3dc95ccb59ede1394ed1a4bff6f5ffac51566
mike <miketz@users.noreply.github.com>
2016-09-13 10:36:20 -0400

    use the loop macro

--- notElpa/mine/my-type-tutor.el
+++ notElpa/mine/my-type-tutor.el
@@ -1,5 +1,6 @@
 (defun my-type-tutor ()
   (interactive)
+  ;; (switch-to-buffer "typing practice")
   (my-type-tutor-defaults
    (list "`" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" "-" "=" ;top row
          "~" "!" "@" "#" "$" "%" "^" "&" "*" "(" ")" "_" "+" ;Shift top row
@@ -11,20 +12,33 @@
         (group-size 9))
     (my-type-tutor-insert-buffer char-lst lines groups-per-line group-size)))
 
+;; (defun my-type-tutor-insert-buffer (chars lines groups-per-line group-size)
+;;   (save-excursion
+;;     (let ((chars-len (- (length chars) 1))
+;;           (l 0))
+;;       (while (< l lines)
+;;         (let ((gpl 0))
+;;           (while (< gpl groups-per-line)
+;;             (let ((gs 0))
+;;               (while (< gs group-size)
+;;                 (insert (my-getAtIndex (rand 0 chars-len)
+;;                                        chars))
+;;                 (incf gs)))
+;;             (insert " ")
+;;             (incf gpl)))
+;;         (insert "\n\n\n")
+;;         (incf l))))
+;;   (next-line))
+
 (defun my-type-tutor-insert-buffer (chars lines groups-per-line group-size)
   (save-excursion
-    (let ((chars-len (- (length chars) 1))
-          (l 0))
-      (while (< l lines)
-        (let ((gpl 0))
-          (while (< gpl groups-per-line)
-            (let ((gs 0))
-              (while (< gs group-size)
-                (insert (my-getAtIndex (rand 0 chars-len)
-                                       chars))
-                (incf gs)))
-            (insert " ")
-            (incf gpl)))
-        (insert "\n\n\n")
-        (incf l))))
+    (loop with chars-len = (1- (length chars))
+          for l from 0 to (1- lines) do
+          (loop for gpl from 0 to (1- groups-per-line) do
+                (loop for gs from 0 to (1- group-size) do
+                      (insert (my-getAtIndex (rand 0 chars-len)
+                                             chars)))
+                (insert " "))
+          (insert "\n\n\n")))
   (next-line))
+

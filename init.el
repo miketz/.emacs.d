6bed605ce86615587d1299d845db811bf44524dc
mike <miketz@users.noreply.github.com>
2016-09-22 10:52:09 -0400

    back to ivy

--- init.el
+++ init.el
@@ -280,7 +280,7 @@ Choices: evil emacs cua")
 Just a convenience to avoid checks agaisnt `my-ui-type'.")
 
 
-(defvar my-narrow-type nil
+(defvar my-narrow-type 'ivy
   "The package I'm currenlty using for narrowing completions.
 Use nil for the emacs default.
 Choices: ivy ido helm icicles sallet nil")

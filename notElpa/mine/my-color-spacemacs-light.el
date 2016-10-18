e3b44e7409d5cfa0c06793209ce070c860bfb121
mike <miketz@users.noreply.github.com>
2016-10-18 18:57:15 -0400

    custom tweaks to spacemacs-light theme

---
+++ notElpa/mine/my-color-spacemacs-light.el
@@ -0,0 +1,27 @@
+(defun my-color-spacemacs-light ()
+  (interactive)
+  (load-theme 'spacemacs-light t)
+
+  (let ((cur '(box "blue")))
+    (setq evil-normal-state-cursor cur)
+    (setq evil-visual-state-cursor '(hollow "blue"))
+    (setq evil-operator-state-cursor cur))
+
+  (custom-theme-set-faces
+   'spacemacs-light
+
+   `(default ((t (:foreground "black" :background "white"))))
+
+   `(show-paren-match ((t (:slant italic
+                                  :bold t
+                                  :strike-through t
+                                  :background nil))))
+
+   `(mode-line ((t (:foreground "#655370" :background "#e7e5eb" :box (:color "#b3b9be"  :line-width -1)))))
+   `(mode-line-inactive ((t (:foreground "#655370" :background "#fbf8ef" :box (:color "#b3b9be" :line-width -1)))))
+
+   ;; `(mode-line ((t (:box (:line-width -1
+   ;;                                    :style released-button)))))
+   ;; `(mode-line-inactive ((t (:box (:line-width -1
+   ;;                                             :sytle pressed-button)))))
+   ))

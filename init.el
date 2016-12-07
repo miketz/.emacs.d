ed3742d895f841ac7d470817c8e030fbbc96676e
mike <miketz@users.noreply.github.com>
2016-12-07 18:05:22 -0500

    alterative to package-initialize
    not used yet. Need to work out an issue where ~/.emacs.d is added
    to the load path.

--- init.el
+++ init.el
@@ -457,6 +457,80 @@ Choices: helm-swoop swiper")
 (package-initialize) ;; activate all the packages (in particular autoloads)
 
 
+
+
+
+
+(defconst my-autoloads-dump-file "~/.emacs.d/all-packages-autoloads.el"
+  "A file to accumulate all the autoloads of packages.
+To avoid searching for autoload files (which is slow).")
+
+(defun my-scrape-out-package-autoloads ()
+  "Looks at every folder under ~/.emacs.d/elpa, scrapes out the autoload code
+from the *-autoloads.el files and copies it into 1 giant dump file. This dump
+file is loaded during emacs start up as an alterative to calling
+`package-initialize'.
+
+The motivation behind this fn is to avoid a very slow call to
+`package-initialize' during every emacs startup. It should be faster to
+load and run 1 known autoload file than search for many unknown autoload files,
+load, and then run them."
+  (interactive)
+  ;; create empty file if it does not exist.
+  (unless (file-exists-p my-autoloads-dump-file)
+    (with-temp-buffer (write-file my-autoloads-dump-file)))
+
+  (with-current-buffer (find-file-noselect my-autoloads-dump-file)
+    ;; clear text in `my-autoloads-dump-file'
+    (goto-char (point-min))
+    (delete-char (1- (point-max)))
+    (save-buffer)
+
+    ;; append autoload code of each package into the dump file
+    (cl-loop for dir in (mapcar #'first
+                                (remove-if (lambda (f)
+                                             (or (my-str-ends-with-p (first f) ".")
+                                                 (my-str-ends-with-p (first f) "..")))
+                                           (remove-if-not (lambda (f)
+                                                            (second f))
+                                                          (directory-files-and-attributes
+                                                           "~/.emacs.d/elpa" t))))
+             do
+             (cl-loop for a in (remove-if-not (lambda (x)
+                                                (and
+                                                 ;; is file
+                                                 (not (second x))
+                                                 ;; is autoload file
+                                                 (my-str-ends-with-p
+                                                  (first x)
+                                                  "-autoloads.el")))
+                                              (directory-files-and-attributes
+                                               dir t))
+                      do
+                      ;; append text to `my-autoloads-dump-file'
+                      (insert "\n")
+                      (insert (format "\n(add-to-list 'load-path \"%s\")\n" dir))
+                      ;; TODO: exclude the add to load-path from the autoload
+                      ;; file as it detects the current folder ~/.emacs.d/
+                      ;; which is not the actual package folder.
+                      (insert (my-get-string-from-file (first a)))))
+    (save-buffer)))
+
+(when nil
+  ;; bootstrap the giant autoloads file thing. Normally fn
+  ;; `my-scrape-out-package-autoloads' will be called manually, not at startup.
+  ;; But on a new emacs installation, call it on startup.
+  (unless (file-exists-p my-autoloads-dump-file)
+    (my-scrape-out-package-autoloads))
+
+  (load my-autoloads-dump-file))
+
+
+
+
+
+
+
 (defun my-install-packages ()
   "Call this function on a new Emacs installation to install packages.
 Installs packages in the list `my-packages'."

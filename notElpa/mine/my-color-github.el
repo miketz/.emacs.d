1b28270bf3868d025b3dc0fad6dc5a05d666c2a2
mike <miketz@users.noreply.github.com>
2017-03-28 13:22:57 -0400

    tweaks to github theme

--- notElpa/mine/my-color-github.el
+++ notElpa/mine/my-color-github.el
@@ -1,13 +1,32 @@
 (defun my-color-github ()
   (interactive)
   (load-theme 'github t)
-  (set-background-color "white")
+
+  (my-cursor-stuff :color-emacs "maroon" :color-evil "blue")
+  (let ((cur '(box "blue")))
+    (setq evil-normal-state-cursor cur)
+    (setq evil-visual-state-cursor '(hollow "blue"))
+    (setq evil-operator-state-cursor cur))
+
+
+  ;; (set-background-color "white")
   (custom-theme-set-faces
    'github
    `(mode-line ((t (:background "grey75"
                                 :foreground "black"
                                 :box (:line-width -1 :style released-button)
                                 :height 1.0))))
+
+   `(avy-lead-face
+     ((t (:foreground "black"
+                      :background "yellow"
+                      :slant normal
+                      :weight normal
+                      :inverse-video nil))))
+
+   '(fringe
+     ((t (:foreground "#9B9B9B" :background "gray90"))))
+
    `(ace-jump-face-foreground
      ((t (:foreground "yellow"
                       :background "black"
@@ -30,4 +49,5 @@
    ;;'(js2-function-param ((t :foreground "green")))
    ;;'(js2-instance-member ((t :foreground "green")))
    ;;'(js2-private-function-call ((t :foreground "green")))
-   ))
+
+   (my-rainbow-parens-light-bg2)))

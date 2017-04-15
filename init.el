9890b1b3aa05c86055298d9bccfd8174353b2818
Mike <miketz@users.noreply.github.com>
2017-04-15 14:26:15 -0400

    autoload for proj-ydnjs

--- init.el
+++ init.el
@@ -3094,7 +3094,8 @@ and indent."
   (let ((lisp-file "my-proj-wild-dog"))
     (autoload #'proj-dive-python lisp-file nil t)
     (autoload #'proj-pcl lisp-file nil t)
-    (autoload #'proj-progit2 lisp-file nil t)))
+    (autoload #'proj-progit2 lisp-file nil t)
+    (autoload #'proj-ydnjs lisp-file nil t)))
 
 (when (eq my-curr-computer 'work-laptop)
   (let ((lisp-file "my-proj-work-laptop" ))

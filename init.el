ae6e67147031bf106e923d050f9d52419d711cb8
mike <miketz@users.noreply.github.com>
2017-06-01 09:10:36 -0400

    use bare-ido

--- init.el
+++ init.el
@@ -280,7 +280,7 @@ Choices: evil emacs cua")
 Just a convenience to avoid checks agaisnt `my-ui-type'.")
 
 
-(defvar my-narrow-type (cond ((eq my-curr-computer 'work-laptop) 'ivy)
+(defvar my-narrow-type (cond ((eq my-curr-computer 'work-laptop) 'bare-ido)
                              ((eq my-curr-computer 'wild-dog) 'ivy)
                              (t bare-ido))
   "The package I'm currenlty using for narrowing completions.

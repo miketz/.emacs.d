710eadf163e736fdd44b20bcb893a491ab5d2235
mike <miketz@users.noreply.github.com>
2017-07-11 10:36:47 -0400

    fn for tweaks to gruvbox-dark theme.

---
+++ notElpa/mine/my-color-gruvbox-dark.el
@@ -0,0 +1,19 @@
+(defun my-color-gruvbox-dark ()
+  (interactive)
+  (load-theme 'gruvbox-dark t)
+
+  (custom-theme-set-variables
+   'gruvbox-dark
+   `(fci-rule-color "#4d4d4d")
+
+   `(evil-emacs-state-cursor '(bar "cyan"))
+   `(evil-normal-state-cursor '(hollow "spring green"))
+   `(evil-insert-state-cursor '(bar "spring green"))
+   `(evil-visual-state-cursor '(hollow "yellow"))
+   `(evil-operator-state-cursor '(hollow "spring green"))
+   `(evil-replace-state-cursor '(hbar "spring green"))
+   `(evil-motion-state-cursor '(box "spring green")))
+
+  (custom-theme-set-faces
+   'gruvbox-dark
+   `(region ((t (:background "#69685E"))))))

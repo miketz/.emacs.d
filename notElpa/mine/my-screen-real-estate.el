adcf17cac76a039e026e69e11f526bd16f8c4654
mike <miketz@users.noreply.github.com>
2017-10-31 12:11:54 -0400

    use lexcial binding. use private lexical var instead of global var.

--- notElpa/mine/my-screen-real-estate.el
+++ notElpa/mine/my-screen-real-estate.el
@@ -1,26 +1,29 @@
+;;; -*- lexical-binding: t -*-
+
 ;; TODO: complete this.
 
 
-(defvar my-backup-mode-line-format nil
-  "Backs up the modeline state so it can later be restored after nilling it out.")
-(defvar my-backup-fringe-mode 0
-  "Backs up the modeline state so it can later be restored after nilling it out.")
+;; (defvar my-backup-mode-line-format nil
+;;   "Backs up the modeline state so it can later be restored after nilling it out.")
+;; (defvar my-backup-fringe-mode 0
+;;   "Backs up the modeline state so it can later be restored after nilling it out.")
 
 
+(let ((backup-format nil)) ;; Backs up the modeline state so it can later be
+                           ;; restored after nilling it out.
+ (defun my-real-estate-hide-mode-line ()
+   (interactive)
+   ;; backup modeline, but only if needed so we don't mess up a good backup.
+   (when (null backup-format)
+     (setq backup-format mode-line-format))
+   ;; disable mode-line for vertical real-estate
+   (setq mode-line-format nil))
 
-(defun my-real-estate-hide-mode-line ()
-  (interactive)
-  ;; backup modeline, but only if needed so we don't mess up a good backup.
-  (when (null my-backup-mode-line-format)
-    (setq my-backup-mode-line-format mode-line-format))
-  ;; disable mode-line for vertical real-estate
-  (setq mode-line-format nil))
-
-(defun my-real-estate-restore-mode-line ()
-  (interactive)
-  ;; restore mode line from backup, but only if there is a good backup.
-  (when (not (null my-backup-mode-line-format))
-    (setq mode-line-format my-backup-mode-line-format)))
+ (defun my-real-estate-restore-mode-line ()
+   (interactive)
+   ;; restore mode line from backup, but only if there is a good backup.
+   (when (not (null backup-format))
+     (setq mode-line-format backup-format))))
 
 
 
@@ -31,9 +34,9 @@
 
 (defun my-real-estate-restore-fringe ()
   (interactive)
-  ;; disabel fringe for horizontal real-estate.
-  (set-fringe-mode nil) ;; nil means the default fringe width. TODO: use a backup value.
-  )
+  ;; disable fringe for more horizontal real-estate.
+  ;; nil means the default fringe width. TODO: use a backup value.
+  (set-fringe-mode nil))
 
 
 

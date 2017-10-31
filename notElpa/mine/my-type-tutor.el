c550e661eeda855e7c85202904717b462292092c
mike <miketz@users.noreply.github.com>
2017-10-31 11:10:40 -0400

    clean up my-type-tutor.el
    delete commented code
    remove function my-type-tutor-defaults.
    formatting

--- notElpa/mine/my-type-tutor.el
+++ notElpa/mine/my-type-tutor.el
@@ -4,35 +4,16 @@
 
 (defun my-type-tutor ()
   (interactive)
-  ;; (switch-to-buffer "typing practice")
-  (my-type-tutor-defaults
-   (list "`" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" "-" "=" ;top row
-         "~" "!" "@" "#" "$" "%" "^" "&" "*" "(" ")" "_" "+" ;Shift top row
-         "[" "]" "{" "}")))
-
-(defun my-type-tutor-defaults (char-lst)
   (let ((lines 7)
         (groups-per-line 7)
-        (group-size 9))
+        (group-size 9)
+        (char-lst
+         (list
+          "`" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" "-" "=" ;top row
+          "~" "!" "@" "#" "$" "%" "^" "&" "*" "(" ")" "_" "+" ;Shift top row
+          "[" "]" "{" "}")))
     (my-type-tutor-insert-buffer char-lst lines groups-per-line group-size)))
 
-;; (defun my-type-tutor-insert-buffer (chars lines groups-per-line group-size)
-;;   (save-excursion
-;;     (let ((chars-len (- (length chars) 1))
-;;           (l 0))
-;;       (while (< l lines)
-;;         (let ((gpl 0))
-;;           (while (< gpl groups-per-line)
-;;             (let ((gs 0))
-;;               (while (< gs group-size)
-;;                 (insert (nth (rand 0 chars-len)
-;;                                        chars))
-;;                 (incf gs)))
-;;             (insert " ")
-;;             (incf gpl)))
-;;         (insert "\n\n\n")
-;;         (incf l))))
-;;   (next-line))
 
 (defun my-type-tutor-insert-buffer (chars lines groups-per-line group-size)
   (save-excursion
@@ -42,7 +23,8 @@
                       (cl-loop for gs from 0 to (1- group-size) do
                                (insert (nth (rand 0 chars-len)
                                             chars)))
-                      (unless (= gpl (1- groups-per-line)) ; skip space on last one
+                      ;; skip space on last one
+                      (unless (= gpl (1- groups-per-line))
                         (insert " ")))
              (insert "\n\n\n")))
   (next-line))

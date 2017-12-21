b0138dce2c8a7bcbb45a095527bc7964f7c49d1b
mike <miketz@users.noreply.github.com>
2017-12-21 10:43:18 -0500

    remember position from eww buffer

--- init.el
+++ init.el
@@ -3273,14 +3273,17 @@ and indent."
 (defun my-proj-pcl-fancy-modes ()
   "Call this while in the eww buffer."
   (interactive)
-  (eww-reload) ; causes images to load that may have failed previously.
-  ;; dynamically override default mor settings
-  (let ((mor-readonly-for-extra-protection-p nil) ; eww is already readonly.
-        (mor-format-automatically-p nil) ; non-lisp text is present.
-        (mor-switch-buff-fn #'switch-to-buffer) ; use same window.
-        (mor-mode-fn #'common-lisp-mode))
-    (mor-mode-on-region (point-min) (point-max))
-    (whitespace-cleanup)))
+  (let ((curr-pos (point))) ; remember position in eww buffer.
+    (eww-reload) ; causes images to load that may have failed previously.
+    ;; dynamically override default mor settings
+    (let ((mor-readonly-for-extra-protection-p nil) ; eww is already readonly.
+          (mor-format-automatically-p nil) ; non-lisp text is present.
+          (mor-switch-buff-fn #'switch-to-buffer) ; use same window.
+          (mor-mode-fn #'common-lisp-mode))
+      (mor-mode-on-region (point-min) (point-max))
+      (whitespace-cleanup)
+      ;; try to warp to generally the same area you were in the eww buffer.
+      (goto-char curr-pos))))
 
 (when (eq my-curr-computer 'wild-dog)
   (let ((lisp-file "my-proj-wild-dog"))

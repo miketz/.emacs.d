fa7779c6af0503081c2d2e22f0d00210eca9219d
mike <miketz@users.noreply.github.com>
2018-02-05 11:14:44 -0500

    ruler

--- init.el
+++ init.el
@@ -6080,6 +6080,42 @@ SCROLL-FN will be `my-scroll-left' or `my-scroll-right'."
 ;;;-----------------------------------------------------------------------------
 ;;; MISC options. Keep this at the bottom
 ;;;-----------------------------------------------------------------------------
+(defun my-insert-ruler ()
+  "Insert text to visually measure width in the buffer.  By columns."
+  ;; TODO: make it recursive, to handle 1000's, millions, etc.
+  (interactive)
+  (let* ((cols 110)
+         (secs-10 (/ cols 10))
+         (remainder-10 (mod cols 10))
+         (secs-100 (/ cols 100)))
+    ;; the 100's
+    (cl-loop for s from 1 to secs-100
+             do
+             (cl-loop for i from 1 to 100
+                      do
+                      (if (< i 100)
+                          (insert " ")
+                        (insert (format "%d" (mod s 100))))))
+    (when (> secs-100 0)
+      (insert "\n"))
+    ;; the 10's
+    (cl-loop for s from 1 to secs-10
+             do
+             (cl-loop for i from 1 to 10
+                      do
+                      (if (< i 10)
+                          (insert " ")
+                        (insert (format "%d" (mod s 10))))))
+    (when (> secs-10 0)
+      (insert "\n"))
+    ;; the 1's
+    (cl-loop repeat secs-10
+             do
+             (insert "1234567890"))
+    (cl-loop for i from 1 to remainder-10
+             do
+             (insert (format "%d" i)))))
+
 (defun my-goto-line (line-num)
   "Docs say don't use `goto-line' in Lisp code.
 Making my own using the reccomended alternative.

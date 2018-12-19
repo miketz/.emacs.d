be2a1f3ee812326cd92fa8a9f5f8ecb1382c8401
mike <miketz@users.noreply.github.com>
2018-12-19 15:24:38 -0500

    comment about support for old emacs versions.

--- initFast.el
+++ initFast.el
@@ -1,3 +1,6 @@
+;;; NOTE: keep this init basic so it works with older Emacs versions.
+;;; Oldest emacs verified: 22.3.1
+
 ;; turn off mouse interface early in startup to avoid momentary display
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))

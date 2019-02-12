a91d3bbfab35e6b372504156af635ef94e409f67
mike <miketz@users.noreply.github.com>
2019-02-12 10:36:08 -0500

    add support for backward cycling of fonts.
    Switch to dynamic binding for vars instead of lexical.
    It's useful to dynamically shadow my-fonts with the reversal so
    I can reuse the same function for both forward and backward.

--- init.el
+++ init.el
@@ -1937,6 +1937,9 @@ Closure over `inverse-video-p'"
 
 
 
+;;;----------------------------------------------------------------------------
+;;; Font cycling stuff.
+;;;----------------------------------------------------------------------------
 (cl-defun my-curr-font-name ()
   "Return the current font name as a string."
   (interactive)
@@ -1945,39 +1948,66 @@ Closure over `inverse-video-p'"
       (cl-return-from my-curr-font-name nil))
     (aref (font-info font-obj) 0)))
 
-(let
-    ((curr-font (my-curr-font-name))
-     (fonts
-      (cond
-       ((eq my-curr-computer 'wild-dog)
-        '("-misc-fixed-medium-r-normal--20-200-75-75-c-100-iso8859-9"
-          "-misc-fixed-bold-r-normal--15-140-75-75-c-90-iso8859-7"
-          "-misc-fixed-medium-r-normal--15-140-75-75-c-90-iso8859-7"
-          "-DAMA-Ubuntu Mono-normal-normal-normal-*-20-*-*-*-m-0-iso10646-1"))
-       ((eq my-curr-computer 'work-laptop)
-        '("-raster-Dina-normal-normal-normal-mono-15-*-*-*-c-*-iso8859-1"
-          "-raster-Fixedsys-normal-normal-normal-mono-17-*-*-*-c-*-iso8859-1"
-          ;; (concat "-raster-ProFontWindows-normal-normal-normal"
-          ;;         "-*-22-*-*-*-c-*-iso8859-1")
-          "-raster-Terminal-normal-normal-normal-mono-12-*-*-*-c-*-ms-oemlatin"
-          "-raster-Terminal-normal-normal-normal-mono-18-*-*-*-c-*-ms-oemlatin"
-          "-raster-r_ansi-normal-normal-normal-mono-15-*-*-*-c-*-iso8859-1"
-          "-raster-r_ansi-normal-normal-normal-mono-17-*-*-*-c-*-iso8859-1"
-          "-outline-Consolas-normal-normal-normal-mono-17-*-*-*-c-*-iso8859-1"
-          "-raster-peep-normal-normal-normal-mono-16-*-*-*-c-*-ms-oemlatin"
-          "-raster-peep-normal-normal-normal-mono-21-*-*-*-c-*-ms-oemlatin"))
-       (t '()))))
+(defvar my-curr-font (my-curr-font-name)
+  "Name of the current font.")
 
-  (defun my-cycle-fonts ()
-    "Cycle through several good fonts for the current computer.
+(defvar my-fonts
+  (cond
+   ((eq my-curr-computer 'wild-dog)
+    '("-misc-fixed-medium-r-normal--20-200-75-75-c-100-iso8859-9"
+      "-misc-fixed-bold-r-normal--15-140-75-75-c-90-iso8859-7"
+      "-misc-fixed-medium-r-normal--15-140-75-75-c-90-iso8859-7"
+      "-DAMA-Ubuntu Mono-normal-normal-normal-*-20-*-*-*-m-0-iso10646-1"))
+   ((eq my-curr-computer 'work-laptop)
+    '("-raster-Dina-normal-normal-normal-mono-15-*-*-*-c-*-iso8859-1"
+      "-raster-Fixedsys-normal-normal-normal-mono-17-*-*-*-c-*-iso8859-1"
+      ;; (concat "-raster-ProFontWindows-normal-normal-normal"
+      ;;         "-*-22-*-*-*-c-*-iso8859-1")
+      "-raster-Terminal-normal-normal-normal-mono-12-*-*-*-c-*-ms-oemlatin"
+      "-raster-Terminal-normal-normal-normal-mono-18-*-*-*-c-*-ms-oemlatin"
+      "-raster-r_ansi-normal-normal-normal-mono-15-*-*-*-c-*-iso8859-1"
+      "-raster-r_ansi-normal-normal-normal-mono-17-*-*-*-c-*-iso8859-1"
+      "-outline-Consolas-normal-normal-normal-mono-17-*-*-*-c-*-iso8859-1"
+      "-raster-peep-normal-normal-normal-mono-16-*-*-*-c-*-ms-oemlatin"
+      "-raster-peep-normal-normal-normal-mono-21-*-*-*-c-*-ms-oemlatin"))
+   (t '()))
+  "Good fonts for the current computer.")
+
+(defun my-get-i (val lst)
+  "Get the index of VAL in LST."
+  (cond ((null lst) 0)
+        ((equal val (car lst)) 0)
+        (t (+ (my-get-i val (cdr lst))
+              1))))
+
+(defun my-cycle-font-forward ()
+  "Cycle through several good fonts for the current computer.
 Closure over `fonts'."
-    (interactive)
-    (setq curr-font (car (or (cdr (member curr-font fonts))
-                             fonts)))
-    (set-frame-font curr-font)
-    (message "font: %s" curr-font)))
+  (interactive)
+  (setq my-curr-font (car (or (cdr (member my-curr-font my-fonts))
+                           my-fonts)))
+  (set-frame-font my-curr-font)
+  (my-print-font))
 
-(global-set-key (kbd "<f8>") #'my-cycle-fonts)
+(defun my-cycle-font-backward ()
+  "Cycle through several good fonts for the current computer backwards.
+Closure over `fonts'."
+  (interactive)
+  (let ((my-fonts (reverse my-fonts)))
+    (my-cycle-font-forward))
+  ;; print outside the let for the original sort
+  (my-print-font))
+
+(defun my-print-font ()
+  "Print the current font.
+Include the current index and length for `my-fonts'."
+  (message "font %d/%d: %s"
+           (1+ (my-get-i my-curr-font my-fonts))
+           (length my-fonts)
+           my-curr-font))
+
+(global-set-key (kbd "<f8>") #'my-cycle-font-forward)
+(global-set-key (kbd "S-<f8>") #'my-cycle-font-backward)
 
 
 ;;;----------------------------------------------------------------------------

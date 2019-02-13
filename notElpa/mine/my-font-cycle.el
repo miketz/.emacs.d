7cb447786e779190ddaaf83757700e9e638618de
mike <miketz@users.noreply.github.com>
2019-02-13 14:29:25 -0500

    switch to an array/index based implementation.
    More effecient than using list operations to move forward and
    reversing a list to move backwards.

--- notElpa/mine/my-font-cycle.el
+++ notElpa/mine/my-font-cycle.el
@@ -8,10 +8,13 @@
 (require 'cl-lib)
 
 (defvar my-curr-computer)
-(declare-function my-cycle-font-forward 'my-font-cycle)
-(declare-function my-cycle-font-backward 'my-font-cycle)
-(declare-function my-print-font 'my-font-cycle)
+;; (declare-function my-cycle-font-forward 'my-font-cycle)
+;; (declare-function my-cycle-font-backward 'my-font-cycle)
+;; (declare-function my-print-font 'my-font-cycle)
+(declare-function my-font-set-index 'my-font-cycle)
+(declare-function my-cycle-font 'my-font-cycle)
 
+;; no longer used.
 (cl-defun my-curr-font-name ()
   "Return the current font name as a string."
   (interactive)
@@ -20,6 +23,7 @@
       (cl-return-from my-curr-font-name nil))
     (aref (font-info font-obj) 0)))
 
+;; no longer used.
 (defun my-get-i (val lst)
   "Get the index of VAL in LST."
   (cond ((null lst) 0)
@@ -27,51 +31,59 @@
         (t (+ (my-get-i val (cdr lst))
               1))))
 
-(let ((my-curr-font (my-curr-font-name))
-      (my-fonts
-       (cond
-        ((eq my-curr-computer 'wild-dog)
-         '("-misc-fixed-medium-r-normal--20-200-75-75-c-100-iso8859-9"
-           "-misc-fixed-bold-r-normal--15-140-75-75-c-90-iso8859-7"
-           "-misc-fixed-medium-r-normal--15-140-75-75-c-90-iso8859-7"
-           "-DAMA-Ubuntu Mono-normal-normal-normal-*-20-*-*-*-m-0-iso10646-1"))
-        ((eq my-curr-computer 'work-laptop)
-         '("-raster-Dina-normal-normal-normal-mono-15-*-*-*-c-*-iso8859-1"
-           "-raster-Fixedsys-normal-normal-normal-mono-17-*-*-*-c-*-iso8859-1"
-           ;; (concat "-raster-ProFontWindows-normal-normal-normal"
-           ;;         "-*-22-*-*-*-c-*-iso8859-1")
-           "-raster-Terminal-normal-normal-normal-mono-12-*-*-*-c-*-ms-oemlatin"
-           "-raster-Terminal-normal-normal-normal-mono-18-*-*-*-c-*-ms-oemlatin"
-           "-raster-r_ansi-normal-normal-normal-mono-15-*-*-*-c-*-iso8859-1"
-           "-raster-r_ansi-normal-normal-normal-mono-17-*-*-*-c-*-iso8859-1"
-           "-outline-Consolas-normal-normal-normal-mono-17-*-*-*-c-*-iso8859-1"
-           "-raster-peep-normal-normal-normal-mono-16-*-*-*-c-*-ms-oemlatin"
-           "-raster-peep-normal-normal-normal-mono-21-*-*-*-c-*-ms-oemlatin"))
-        (t '()))))
+(let*
+    ((my-fonts
+      (cond
+       ((eq my-curr-computer 'wild-dog)
+        '["-misc-fixed-medium-r-normal--20-200-75-75-c-100-iso8859-9"
+          "-misc-fixed-bold-r-normal--15-140-75-75-c-90-iso8859-7"
+          "-misc-fixed-medium-r-normal--15-140-75-75-c-90-iso8859-7"
+          "-DAMA-Ubuntu Mono-normal-normal-normal-*-20-*-*-*-m-0-iso10646-1"])
+       ((eq my-curr-computer 'work-laptop)
+        '["-raster-Dina-normal-normal-normal-mono-15-*-*-*-c-*-iso8859-1"
+          "-raster-Fixedsys-normal-normal-normal-mono-17-*-*-*-c-*-iso8859-1"
+          ;; (concat "-raster-ProFontWindows-normal-normal-normal"
+          ;;         "-*-22-*-*-*-c-*-iso8859-1")
+          "-raster-Terminal-normal-normal-normal-mono-12-*-*-*-c-*-ms-oemlatin"
+          "-raster-Terminal-normal-normal-normal-mono-18-*-*-*-c-*-ms-oemlatin"
+          "-raster-r_ansi-normal-normal-normal-mono-15-*-*-*-c-*-iso8859-1"
+          "-raster-r_ansi-normal-normal-normal-mono-17-*-*-*-c-*-iso8859-1"
+          "-outline-Consolas-normal-normal-normal-mono-17-*-*-*-c-*-iso8859-1"
+          "-raster-peep-normal-normal-normal-mono-16-*-*-*-c-*-ms-oemlatin"
+          "-raster-peep-normal-normal-normal-mono-21-*-*-*-c-*-ms-oemlatin"])
+       (t nil)))
+     (i 0)
+     (len (length my-fonts)))
 
-  (defun my-cycle-font-forward (&optional font-lst)
-    "Cycle through several good fonts for the current computer.
-Closure over `fonts'."
-    (interactive)
-    (let ((my-fonts (or font-lst my-fonts)))
-      (setq my-curr-font (car (or (cdr (member my-curr-font my-fonts))
-                                  my-fonts))))
-    (set-frame-font my-curr-font)
-    (my-print-font))
-
-  (defun my-cycle-font-backward ()
-    "Cycle through several good fonts for the current computer backwards.
-Closure over `fonts'."
-    (interactive)
-    (my-cycle-font-forward (reverse my-fonts)))
-
-  (defun my-print-font ()
-    "Print the current font.
-Include the current index and length for `my-fonts'."
+  (defun my-font-set-index (new-i)
+    "Set the current font index and display that font.
+Closure over `my-fonts'."
+    (interactive "n") ; read new index from minibuffer if not provided.
+    (setq i (if (or (>= new-i len)
+                    (< new-i 0))
+                (mod new-i len) ; if i out of range then wrap via mod.
+              new-i))           ; else good index
+    (set-frame-font (aref my-fonts i))
     (message "font %d/%d: %s"
-             (1+ (my-get-i my-curr-font my-fonts))
-             (length my-fonts)
-             my-curr-font)))
+             (1+ i)
+             len
+             (aref my-fonts i)))
+
+  (defun my-cycle-font (step)
+    "Cycle through several good fonts for the current computer by N STEPs.
+Closure over `my-fonts'."
+    (interactive "n") ; read step from minibuffer if not provided.
+    (my-font-set-index (+ i step))))
+
+(defun my-cycle-font-forward ()
+  "Cycle through several good fonts for the current computer."
+  (interactive)
+  (my-cycle-font 1))
+
+(defun my-cycle-font-backward ()
+  "Cycle through several good fonts for the current computer backwards."
+  (interactive)
+  (my-cycle-font -1))
 
 
 (provide 'my-font-cycle)

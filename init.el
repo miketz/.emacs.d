7eb45667b9f41dcbc25f6f8cf3d938ab57e244c9
mike <miketz@users.noreply.github.com>
2019-03-04 17:59:23 -0500

    delete custom-set-variables section for raspberry-pi

--- init.el
+++ init.el
@@ -1615,28 +1615,6 @@ Closure over `curr-font-size'."
 ;;     (my-set-font :weight weight
 ;;                  :resize-window t)))
 
-(when (eq my-curr-computer 'raspberry-pi)
-  (custom-set-variables
-   ;; custom-set-variables was added by Custom.
-   ;; If you edit it by hand, you could mess it up, so be careful.
-   ;; Your init file should contain only one such instance.
-   ;; If there is more than one, they won't work right.
-   '(ansi-color-faces-vector
-     [default default default italic underline success warning error])
-   '(ansi-color-names-vector
-     ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue"
-      "gray50"])
-   '(font-use-system-font t)
-   '(tool-bar-mode nil))
-  (custom-set-faces
-   ;; custom-set-faces was added by Custom.
-   ;; If you edit it by hand, you could mess it up, so be careful.
-   ;; Your init file should contain only one such instance.
-   ;; If there is more than one, they won't work right.
-   '(default ((t (:family "Fixed" :foundry "Misc"
-                          :slant normal :weight normal :height 150
-                          :width normal))))))
-
 
 
 ;;;----------------------------------------------------------------------------

64a3fef1dae51e7ec050c7d1b5cf6966a2c8c05c
Zaleski <mtz@NIST.GOV>
2019-03-27 10:53:17 -0400

    select numbered completions in company

--- init.el
+++ init.el
@@ -2079,6 +2079,36 @@ This avoids changing pop-up width while scrolling through candidates."
   (setq company-tooltip-limit 21))       ; popup more suggestions.
 
 
+;;;----------------------------------------------------------------------------
+;;; select numbered completions. from https://oremacs.com/
+;;;----------------------------------------------------------------------------
+(setq company-show-numbers t)
+
+(defun ora-company-number ()
+  "Forward to `company-complete-number'.
+
+Unless the number is potentially part of the candidate.
+In that case, insert the number."
+  (interactive)
+  (let* ((k (this-command-keys))
+         (re (concat "^" company-prefix k)))
+    (if (cl-find-if (lambda (s) (string-match re s))
+                    company-candidates)
+        (self-insert-command 1)
+      (company-complete-number (string-to-number k)))))
+
+(let ((map company-active-map))
+  (mapc
+   (lambda (x)
+     (define-key map (format "%d" x) 'ora-company-number))
+   (number-sequence 0 9))
+  (define-key map " " (lambda ()
+                        (interactive)
+                        (company-abort)
+                        (self-insert-command 1)))
+  (define-key map (kbd "<return>") nil))
+
+
 
 ;;;----------------------------------------------------------------------------
 ;;; company-web

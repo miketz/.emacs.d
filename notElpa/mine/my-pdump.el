ba44ade4df8542592a7628ab3ebc85a8c5d412c4
Zaleski <mtz@NIST.GOV>
2019-04-09 17:52:15 -0400

    require files lib before pdump

--- notElpa/mine/my-pdump.el
+++ notElpa/mine/my-pdump.el
@@ -127,6 +127,7 @@ For faster subsequent start up."
     (require 'info)
     (require 'browse-url)
     (require 'goto-addr)
+    (require 'files)
     ;; (require 'winner) ; exclude winner. It breaks the dump.
     ;; (require 'magit) ; exclude magit until hook performance is resolved.
 

278c52f17cb3cb230cf99f83c57ff6bcba065748
mike <miketz@users.noreply.github.com>
2019-08-14 16:10:04 -0400

    file to make a pdump. intented to be run via emacs --batch

---
+++ notElpa/mine/my-make-pdump.el
@@ -0,0 +1,21 @@
+;;;----------------------------------------------------------------------------
+;;; make a pdump.
+;;;
+;;; to make a dump:
+;;;     emacs --batch -l ~./emacs.d/notElpa/mine/my-make-pdump.el
+;;;
+;;; run with the dump
+;;;     emacs --dump-file=~/.emacs.d/dump.pdmp
+;;;----------------------------------------------------------------------------
+
+;; load init file
+(load "~/.emacs.d/init.el")
+
+;; load my helper functions for dumping.
+(require 'my-pdump)
+
+;; load many libraries.
+(my-load-everything-for-pdump)
+
+;; dump
+(my-make-pdump)

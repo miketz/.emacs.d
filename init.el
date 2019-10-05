996fad707a389805cc54f9080997fd929003461d
mike <miketz@users.noreply.github.com>
2019-10-05 06:49:55 -0400

    add early-init.el
    set package-enable-at-startup nil in early-init.el.
    
    for eamcs 27+ disable gui features in the early-init.el to avoid
    momentary display.

--- init.el
+++ init.el
@@ -109,10 +109,11 @@
   )
 
 ;; Turn off mouse interface early in startup to avoid momentary display
-(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-;;(when (fboundp 'horizontal-scroll-bar-mode) (horizontal-scroll-bar-mode -1))
+(when (version< emacs-version "27.0") ; executes in early-init.el for emacs 27+
+  (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+  (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+  (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+  (when (fboundp 'horizontal-scroll-bar-mode) (horizontal-scroll-bar-mode -1)))
 
 
 

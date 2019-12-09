a2044ff926000325d5007469a36d03f39edb113b
mike <miketz@users.noreply.github.com>
2019-12-09 17:42:35 -0500

    unit tests for rand

--- notElpa/mine/my-rand.el
+++ notElpa/mine/my-rand.el
@@ -42,6 +42,29 @@
 ;;     (insert (int-to-string (elt counts x)))
 ;;     (insert "\n\n")))
 
+
+(ert-deftest rand-range-overflow-test ()
+  "Make sure `rand' generates values within range."
+  (random t) ; seed
+  (let* ((min 3) ; NOTE: small range [3-7]
+         (max 7)
+         ;; generate a ton of sample data over a small range.
+         ;; to increase likelyhood an out of range bug will occur.
+         (data (cl-loop repeat 1000 collect (rand min max))))
+    (cl-flet ((out-of-range (r) (or (< r min) (> r max))))
+      (should-not
+       (cl-member-if #'out-of-range data)))))
+
+(ert-deftest rand-range-underflow-test ()
+  "Make sure `rand' does not have an 'underflow' range bug."
+  (random t) ; seed
+  (let* ((min 3) ; NOTE: small range [3-7]
+         (max 7)
+         (data (cl-loop repeat 1000 collect (rand min max))))
+    (cl-loop for i from min to max
+             do
+             (should (member i data)))))
+
 (provide 'my-rand)
 
 ;;; my-rand.el ends here

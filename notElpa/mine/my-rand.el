f0b63ec77ed363614dd26a3a9f115a7b1ac17636
mike <miketz@users.noreply.github.com>
2019-12-19 14:45:24 -0500

    add rand-occur-count-test

--- notElpa/mine/my-rand.el
+++ notElpa/mine/my-rand.el
@@ -58,6 +58,51 @@
 ;;                (insert " !!!")))
 ;;            (insert "\n\n")))
 
+(ert-deftest rand-occur-count-test ()
+  "Make sure each number in the range occurs a reasonable # of times."
+  (let* ((min 1)
+         (max 20)
+         (rng (1+ (- max min)))
+         (times 250000)
+         (target-occurs (/ times (* 1.0 rng)))
+         (rVals (loop repeat times
+                      collect (rand min max)))
+         (sum (apply #'+ rVals))
+         (len times)                    ; (len (length rVals))
+         (avg (/ sum (* 1.0 len)))
+         (counts (loop for n from min to max
+                       collect 0))
+         (thresh-off-perc 0.05)
+         (thresh-off (* target-occurs thresh-off-perc)))
+    ;; count the distribution
+    (dolist (r rVals)
+      (let ((index (- r 1)))
+        (setf (elt counts index)
+              (+ 1 (elt counts index)))))
+    ;; ;; print
+    ;; (insert (concat "avg: "
+    ;;                 (number-to-string avg)
+    ;;                 "\n"))
+    ;; (insert "target: " (number-to-string target-occurs))
+    ;; (insert "\n")
+    ;; (insert "thresh-off-perc: " (number-to-string thresh-off-perc))
+    ;; (insert "\n")
+    ;; (insert "thresh-off: " (number-to-string thresh-off))
+    ;; (insert "\n")
+    (cl-loop for x from min to max
+             do
+             ;; (insert (number-to-string x))
+             ;; (insert ": ")
+             ;; (insert (number-to-string (elt counts (1- x))))
+             (let ((off-by (- target-occurs (elt counts (1- x)))))
+               ;; (insert (format ", off by: %d" off-by))
+               (should (<= off-by thresh-off))
+               ;; (when (> off-by thresh-off)
+               ;;   (insert " !!!")))
+               )
+             ;; (insert "\n\n")
+             )))
+
 (ert-deftest rand-range-overflow-test ()
   "Make sure `rand' generates values within range."
   (random t) ; seed

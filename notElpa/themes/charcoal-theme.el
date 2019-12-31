a9dfd8c954b4c9d022b1a3e16db9ae9d620a959b
mike <miketz@users.noreply.github.com>
2019-12-31 16:23:20 -0500

    leerzeichen face

--- notElpa/themes/charcoal-theme.el
+++ notElpa/themes/charcoal-theme.el
@@ -527,6 +527,9 @@ Example:
    `(ert-test-result-expected ((,class :foreground ,fg-green :background ,bg-green)))
    `(ert-test-result-unexpected ((,class :foreground ,fg-red :background ,bg-red)))
 
+   ;; leerzeichen
+   `(leerzeichen ((t (:foreground ,faint-less)))) ;;"yellow4" ;"#A8A800"
+
    `(rainbow-delimiters-depth-1-face
      ((,class (:foreground ,rain-1 :background ,rain-1-bg))))
    `(rainbow-delimiters-depth-2-face

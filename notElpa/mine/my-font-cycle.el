63df3bd274c362ba8beb80f3f764bf49fc1d8848
mike <miketz@users.noreply.github.com>
2020-04-13 17:03:04 -0400

    add proggy font

--- notElpa/mine/my-font-cycle.el
+++ notElpa/mine/my-font-cycle.el
@@ -49,6 +49,7 @@
           "-outline-Consolas-normal-normal-normal-mono-17-*-*-*-c-*-iso8859-1"
           "-outline-Consolas-bold-normal-normal-mono-17-*-*-*-c-*-iso8859-1"
           ;; "-raster-peep-normal-normal-normal-mono-16-*-*-*-c-*-ms-oemlatin"
+          "-raster-ProggySquare-normal-normal-normal-mono-11-*-*-*-c-*-iso8859-1"
           "-outline-Ubuntu Mono-normal-normal-normal-mono-*-*-*-*-c-*-iso10646-1"
           "-outline-Ubuntu Mono-bold-normal-normal-mono-*-*-*-*-c-*-iso10646-1"
           "-outline-Lucida Console-normal-normal-normal-mono-*-*-*-*-c-*-tis620-2533"

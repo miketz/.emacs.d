dc3f6119f994b5667383c931090740a07de68109
mike <miketz@users.noreply.github.com>
2020-04-21 22:04:53 -0400

    more examples, fix comments

--- notElpa/mine/my-byte-compiler-tricks.el
+++ notElpa/mine/my-byte-compiler-tricks.el
@@ -7,13 +7,29 @@
 ;; from Misha_B on #emacs
 ;; A macro to wrap things to force them to be evaluted during macro-expansion
 ;; time (ie byte-compile time).
-;; Might only work for things that are actually Values. Would not work for
-;; side-effects like setting the color theme.
+;; Might only work for things that are pure data Values.
+;; Would not work for side-effects like setting the color theme as that must
+;; occur at runtime in the future, not at byte-compile time.
 (defmacro constexpr (b)
   (eval b))
 
 
 ;; in byte-comiled codes, it only runs during byte-compile stage.
-;; so can be used to (require 'some-package) to silence byte-compiler warnigns,
-;; but not actually required the package during init time.
-(eval-when-compile)
+;; any side-effect code in section not run at all when loading init.elc ???
+(eval-when-compile
+  ;; stuff
+  )
+
+
+;; similar to eval-when-compile, but will won't run in non-byte-compiled code?
+;; Can be used to (require 'some-package) to silence byte-compiler warnings,
+;; about free vars during compile time, but not actually require the package
+;; during init time.
+(eval-when 'compile
+  (require 'autorevert))
+;; this techinique shoudl be better than the (defvar foo) solution used
+;; to silence free var warnigns. I can mistype name or package can remove
+;; definition and compilation checker would be silent in such cases
+
+
+

e9945e2f3295ae4fc451e9c71561577f09df3249
mike <miketz@users.noreply.github.com>
2021-01-26 07:30:34 -0500

    change color/font on work-laptop-2019

--- init.el
+++ init.el
@@ -1760,17 +1760,17 @@ This prevents overlapping themes; something I would rarely want."
    default-frame-alist))
 
  ((eq my-curr-computer 'work-laptop-2019)
-  (load-theme 'charcoal t)
+  (load-theme 'ultimate t)
   ;; (set-background-color "#E5E1C3")
-  ;; (set-background-color "ivory2")
+  (set-background-color "ivory3")
   (when my-graphic-p
     (push
      '(font
        .
        ;; "-raster-Fixedsys-normal-normal-normal-mono-15-*-*-*-c-*-iso8859-1"
-       "-raster-Terminal-normal-normal-normal-mono-18-*-*-*-c-*-ms-oemlatin"
+       ;; "-raster-Terminal-normal-normal-normal-mono-18-*-*-*-c-*-ms-oemlatin"
        ;; "-outline-Lucida Console-normal-normal-normal-mono-13-*-*-*-c-*-iso10646-1"
-       ;; "-outline-Ubuntu Mono-bold-normal-normal-mono-16-*-*-*-c-*-iso10646-1"
+       "-outline-Ubuntu Mono-bold-normal-normal-mono-16-*-*-*-c-*-iso10646-1"
        )
      default-frame-alist)
     ;; (set-frame-font

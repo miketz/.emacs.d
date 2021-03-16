a9e30cf92b2d4e76b8eac2f0969a7f307c7ec652
mike <miketz@users.noreply.github.com>
2021-03-16 19:55:45 -0400

    turn off global-auto-revert-mode

--- init.el
+++ init.el
@@ -7063,10 +7063,10 @@ Closure over `preceding-sexp-fn'."
   (setq global-auto-revert-non-file-buffers nil)
   (setq auto-revert-remote-files nil))
 
-;; reload buffer if it changes on disk outside emacs.
-(global-auto-revert-mode t)
-;; auto refresh dired when file changes
-(add-hook 'dired-mode-hook #'auto-revert-mode)
+;; ;; reload buffer if it changes on disk outside emacs.
+;; (global-auto-revert-mode t)
+;; ;; auto refresh dired when file changes
+;; (add-hook 'dired-mode-hook #'auto-revert-mode)
 
 
 ;;;----------------------------------------------------------------------------

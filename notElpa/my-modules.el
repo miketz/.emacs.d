e8d0eb5680f9d570e6db5750b03ffc009230150e
mike <miketz@users.noreply.github.com>
2021-06-23 17:39:11 -0400

    module info for leerzeichen

--- notElpa/my-modules.el
+++ notElpa/my-modules.el
@@ -537,6 +537,20 @@ style. More importantly it avoids spamming rg as you type or prematurely."
       :use-branch "master"
       :depend-hard '()
       :depend-soft '()
+      :depend-bundled '())
+    ,(make-module
+      :name 'leerzeichen
+      :comment nil
+      :folder (concat my-module-folder "leerzeichen.el")
+      :remote-mine-url "https://github.com/miketz/leerzeichen.el"
+      :remote-mine-alias "origin"
+      :remote-upstream-url "https://github.com/fgeller/leerzeichen.el"
+      :remote-upstream-alias "upstream"
+      :source-control 'git
+      :submodule-p t
+      :use-branch "mine"
+      :depend-hard '()
+      :depend-soft '()
       :depend-bundled '())))
 
 (defun my-byte-compile-all-modules ()

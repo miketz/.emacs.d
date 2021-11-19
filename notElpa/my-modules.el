2792d9405e776745f24dba0d046d6a9ee7a34c58
mike <miketz@users.noreply.github.com>
2021-11-19 15:53:40 -0500

    module info for smart-tabs-mode

--- notElpa/my-modules.el
+++ notElpa/my-modules.el
@@ -667,6 +667,20 @@ style. More importantly it avoids spamming rg as you type or prematurely."
                      (seq "1.11")
                      (emacs "24.3"))
       :depend-soft '()
+      :depend-bundled '())
+    ,(make-module
+      :name 'smart-tabs-mode
+      :comment nil
+      :folder (concat my-module-folder "smarttabs")
+      :remote-mine-url "https://github.com/miketz/smarttabs"
+      :remote-mine-alias "origin"
+      :remote-upstream-url "https://github.com/jcsalomon/smarttabs"
+      :remote-upstream-alias "upstream"
+      :source-control 'git
+      :submodule-p t
+      :use-branch "master"
+      :depend-hard '()
+      :depend-soft '()
       :depend-bundled '())))
 
 (defun my-byte-compile-all-modules ()

8a9e6f1270e6f0c585a08c2d1ae8b8f42bcc11e5
mike <miketz@users.noreply.github.com>
2022-02-12 09:06:56 -0500

    more module info

--- notElpa/my-modules.el
+++ notElpa/my-modules.el
@@ -723,6 +723,34 @@ style. More importantly it avoids spamming rg as you type or prematurely."
       :use-branch "master"
       :depend-hard '((emacs "26.1"))
       :depend-soft '()
+      :depend-bundled '())
+    ,(make-module
+      :name 'ace-link
+      :comment nil
+      :folder (concat my-module-folder "ace-link")
+      :remote-mine-url "https://github.com/miketz/ace-link"
+      :remote-mine-alias "origin"
+      :remote-upstream-url "https://github.com/abo-abo/ace-link"
+      :remote-upstream-alias "upstream"
+      :source-control 'git
+      :submodule-p t
+      :use-branch "master"
+      :depend-hard '((avy "0.4.0"))
+      :depend-soft '((counsel) (w3m))
+      :depend-bundled '())
+    ,(make-module
+      :name 'ace-window
+      :comment nil
+      :folder (concat my-module-folder "ace-window")
+      :remote-mine-url "https://github.com/miketz/ace-window"
+      :remote-mine-alias "origin"
+      :remote-upstream-url "https://github.com/abo-abo/ace-window"
+      :remote-upstream-alias "upstream"
+      :source-control 'git
+      :submodule-p t
+      :use-branch "master"
+      :depend-hard '((avy "0.5.0"))
+      :depend-soft '()
       :depend-bundled '())))
 
 (defun my-byte-compile-all-modules ()

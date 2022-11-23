fcdb381c82fbca60cfcc29c9a133bcce2fd8e45b
mike <miketz@users.noreply.github.com>
2022-11-23 14:50:35 -0500

    module info for php-mode

--- notElpa/my-modules.el
+++ notElpa/my-modules.el
@@ -1336,6 +1336,21 @@ style. More importantly it avoids spamming rg as you type or prematurely."
       :depend-soft '()
       :depend-bundled '())
     ,(make-module
+      :name 'php-mode
+      :comment nil
+      :folder (concat my-module-folder "php-mode")
+      :remotes '((mine :url "https://github.com/miketz/php-mode"
+                       :alias "origin")
+                 (upstream :url "https://github.com/emacs-php/php-mode"
+                           :alias "upstream"))
+      :remote-default 'mine
+      :source-control 'git
+      :submodule-p t
+      :use-branch "master"
+      :depend-hard '()
+      :depend-soft '()
+      :depend-bundled '())
+    ,(make-module
       :name 'highlight-indent-guides
       :comment nil
       :folder (concat my-module-folder "highlight-indent-guides")

d12d6cfde20fd1cf92d6518a410c8d0db3435c39
mike <miketz@users.noreply.github.com>
2022-11-23 15:02:03 -0500

    module info for powershell

--- notElpa/my-modules.el
+++ notElpa/my-modules.el
@@ -1366,6 +1366,21 @@ style. More importantly it avoids spamming rg as you type or prematurely."
       :depend-soft '()
       :depend-bundled '())
     ,(make-module
+      :name 'powershell
+      :comment nil
+      :folder (concat my-module-folder "powershell.el")
+      :remotes '((mine :url "https://github.com/miketz/powershell.el"
+                       :alias "origin")
+                 (upstream :url "https://github.com/jschaf/powershell.el"
+                           :alias "upstream"))
+      :remote-default 'mine
+      :source-control 'git
+      :submodule-p t
+      :use-branch "master"
+      :depend-hard '()
+      :depend-soft '()
+      :depend-bundled '())
+    ,(make-module
       :name 'highlight-indent-guides
       :comment nil
       :folder (concat my-module-folder "highlight-indent-guides")

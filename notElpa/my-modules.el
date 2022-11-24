88f65ca36d1b251afe838c10f6e9ebeb0f4bd94f
mike <miketz@users.noreply.github.com>
2022-11-24 09:21:05 -0500

    module info for vimrc-mode

--- notElpa/my-modules.el
+++ notElpa/my-modules.el
@@ -1423,6 +1423,21 @@ style. More importantly it avoids spamming rg as you type or prematurely."
       :depend-soft '()
       :depend-bundled '())
     ,(make-module
+      :name 'vimrc-mode
+      :comment nil
+      :folder (concat my-module-folder "vimrc-mode")
+      :remotes '((mine :url "https://github.com/miketz/vimrc-mode"
+                       :alias "origin")
+                 (upstream :url "https://github.com/mcandre/vimrc-mode"
+                           :alias "upstream"))
+      :remote-default 'mine
+      :source-control 'git
+      :submodule-p t
+      :use-branch "mine"
+      :depend-hard '()
+      :depend-soft '()
+      :depend-bundled '())
+    ,(make-module
       :name 'unkillable-scratch
       :comment nil
       :folder (concat my-module-folder "unkillable-scratch")

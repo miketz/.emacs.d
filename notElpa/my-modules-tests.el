3275e40d9ce55f55495f103b5d69edc6fdf43272
mike <miketz@users.noreply.github.com>
2022-11-28 11:58:52 -0500

    comments about the tests relying on my specific git state

--- notElpa/my-modules-tests.el
+++ notElpa/my-modules-tests.el
@@ -3,8 +3,12 @@
 ;;; Commentary:
 ;;; Tests for package `my-modules'.
 ;;;
+;;; NOTE: These are not "good" unit tests.  They assume specific git repo
+;;; states that only exist on my machine.  They are for my personal testing
+;;; only.
+;;;
 ;;; Naming convention:
-;;;  - prefix all test names with "my-".
+;;;  - prefix all test names with "my-modules-".
 ;;;  - include the word "test" in the name.
 ;;;
 ;;; Run tests:

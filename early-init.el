0674e785118ed75afab3379444b5f50cf2006725
mike <miketz@users.noreply.github.com>
2023-02-10 12:44:41 -0500

    incldue width and height in default-frame-alist sample

--- early-init.el
+++ early-init.el
@@ -3,7 +3,10 @@
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode 0))
 (when (fboundp 'horizontal-scroll-bar-mode) (horizontal-scroll-bar-mode 0))
 
-;; on mac thius makes the window a fixed size and too small!
-;; (add-to-list 'default-frame-alist '(undecorated . t))
+;; on mac thius makes the window a fixed size!!! Rectangle plugin can't resize emacs.
+;; (progn
+;;   (add-to-list 'default-frame-alist '(undecorated . t))
+;;   (add-to-list 'default-frame-alist '(width . 1.0))
+;;   (add-to-list 'default-frame-alist '(height . 1.0)))
 
 (setq package-enable-at-startup nil)

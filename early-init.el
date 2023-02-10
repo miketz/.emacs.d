e547a13943de64c01119124434fcb4737efcccbd
mike <miketz@users.noreply.github.com>
2023-02-10 10:34:51 -0500

    commented code. removes titlte bar.
    But on mac it seems to make the window non-resizable and it's too small!!!
    I would like to remove the title bar eventually though so keep this as
    a comment.

--- early-init.el
+++ early-init.el
@@ -3,4 +3,7 @@
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode 0))
 (when (fboundp 'horizontal-scroll-bar-mode) (horizontal-scroll-bar-mode 0))
 
+;; on mac thius makes the window a fixed size and too small!
+;; (add-to-list 'default-frame-alist '(undecorated . t))
+
 (setq package-enable-at-startup nil)

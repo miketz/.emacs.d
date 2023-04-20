3e28750aff5c65efcaf9ab724ee3a4ef92cd4336
mike <miketz@users.noreply.github.com>
2023-04-20 08:09:20 -0400

    make vars for line number colors. and pick colors for 256 color mode.

--- notElpa/themes/charcoal-theme.el
+++ notElpa/themes/charcoal-theme.el
@@ -70,6 +70,10 @@ Example:
        (fn-def-bg    (aref `["#000000" "#000000" ,todo--fg ,todo--fg] i))
        (fn-call-fg   (aref `["#EFB0BB" "#ffafd7" ,todo--fg ,todo--fg] i))
        (fn-call-bg   (aref `["#232319" "#000000" ,todo--bg ,todo--bg] i))
+       (linenum-fg   (aref `["#595959" "#626262" ,todo--bg ,todo--bg] i))
+       (linenum-bg   (aref `["#231808" "#000000" ,todo--bg ,todo--bg] i))
+       (linenumcur-fg (aref `["#77AA55" "#77AA55" ,todo--bg ,todo--bg] i))
+       (linenumcur-bg (aref `["#231808" "#000000" ,todo--bg ,todo--bg] i))
        (rain-1       (aref `["#FF4500" "#FF0000" "#FF0000" "#FF0000"] i))
        (rain-1-bg    (aref `[,bg       ,bg       ,bg       ,bg      ] i))
        (rain-2       (aref `["#00FFFF" "#00FFFF" "#00FFFF" "#00FFFF"] i))
@@ -203,12 +207,12 @@ Example:
 
    `(line-number ((,class ,@(cond ((= charcoal-color-cnt 16)
                                      `(:inherit mode-line))
-                                  (t `(:background "#231808";"#251510"
-                                       :foreground "gray35" ;"gray50"
+                                  (t `(:background ,linenum-bg
+                                       :foreground ,linenum-fg
                                        ))))))
    `(line-number-current-line ((,class (:inherit line-number
-                                                 :foreground "#77AA55"
-                                                 :background "#231808";"#171717"
+                                                 :foreground ,linenumcur-fg
+                                                 :background ,linenumcur-bg
                                                  ))))
 
    `(region

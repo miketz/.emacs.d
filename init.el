921d4f7dc133ad236b46b241abaa74014449589d
mike <miketz@users.noreply.github.com>
2023-05-31 08:17:03 -0400

    configure treesit grammar urls for several languages

--- init.el
+++ init.el
@@ -8390,7 +8390,28 @@ vanilla javascript buffers."
 ;;;----------------------------------------------------------------------------
 (with-eval-after-load 'treesit
   (setq treesit-font-lock-level 4) ;; color more things
-  )
+
+  ;; repo info for compiling language grammars via
+  ;; `treesit-install-language-grammar'
+  (setq treesit-language-source-alist
+        '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+          (cmake "https://github.com/uyha/tree-sitter-cmake")
+          (css "https://github.com/tree-sitter/tree-sitter-css")
+          (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+          (go "https://github.com/tree-sitter/tree-sitter-go")
+          (html "https://github.com/tree-sitter/tree-sitter-html")
+          (javascript "https://github.com/tree-sitter/tree-sitter-javascript"
+                      "master" "src")
+          (json "https://github.com/tree-sitter/tree-sitter-json")
+          (make "https://github.com/alemuller/tree-sitter-make")
+          (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+          (python "https://github.com/tree-sitter/tree-sitter-python")
+          (toml "https://github.com/tree-sitter/tree-sitter-toml")
+          (tsx "https://github.com/tree-sitter/tree-sitter-typescript"
+               "master" "tsx/src")
+          (typescript "https://github.com/tree-sitter/tree-sitter-typescript"
+                      "master" "typescript/src")
+          (yaml "https://github.com/ikatyang/tree-sitter-yaml"))))
 
 ;;;----------------------------------------------------------------------------
 ;;; tree-sitter, tree-sitter-langs. 3rd party package. works for pre-Emacs 29

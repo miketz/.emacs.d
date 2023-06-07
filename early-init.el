56813afe0ef143cd595d56869ed62df43efd175a
mike <miketz@users.noreply.github.com>
2023-06-07 09:44:04 -0400

    faster way to turn off toolbar?

--- early-init.el
+++ early-init.el
@@ -1,5 +1,7 @@
 (when (fboundp 'menu-bar-mode) (menu-bar-mode 0))
-(when (fboundp 'tool-bar-mode) (tool-bar-mode 0))
+;; (when (fboundp 'tool-bar-mode) (tool-bar-mode 0))
+;; supposedly this is faster than (tool-bar-mode 0)
+(push '(tool-bar-lines . 0) default-frame-alist)
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode 0))
 (when (fboundp 'horizontal-scroll-bar-mode) (horizontal-scroll-bar-mode 0))
 

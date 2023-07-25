bad9b141e7c020b30b2015a372685fc4bd2a36db
mike <miketz@users.noreply.github.com>
2023-07-25 19:23:30 -0400

    use my-compile-at-root-dir for go

--- init.el
+++ init.el
@@ -9171,8 +9171,8 @@ And turns off `indent-tabs-mode'."
 
 (with-eval-after-load 'go-ts-mode
   ;; key binds
-  (define-key go-ts-mode-map (kbd "C-c C-c") #'compile)
-  (define-key go-ts-mode-map (kbd "C-c c") #'compile)
+  (define-key go-ts-mode-map (kbd "C-c C-c") #'my-compile-at-root-dir)
+  (define-key go-ts-mode-map (kbd "C-c c") #'my-compile-at-root-dir)
 
   (define-key go-ts-mode-map (kbd "C-c C-d d") #'eldoc-print-current-symbol-info)
   (define-key go-ts-mode-map (kbd "C-c C-d C-d") #'eldoc-print-current-symbol-info)
@@ -9183,10 +9183,11 @@ And turns off `indent-tabs-mode'."
   (defvar my-gofmt-installed-p (executable-find "gofmt"))
 
   (defun my-setup-go-ts-mode ()
-    (when buffer-file-name ;; if buffer has a file on disk.
-      ;; wireup M-x compile. TODO: revisit this
-      (set (make-local-variable 'compile-command)
-           (concat "go run " (shell-quote-argument buffer-file-name))))
+    ;; (when buffer-file-name ;; if buffer has a file on disk.
+    ;;   ;; wireup M-x compile. TODO: revisit this
+    ;;   (set (make-local-variable 'compile-command)
+    ;;        (concat "go run " (shell-quote-argument buffer-file-name))))
+
     ;; Run gofmt on save. Use "local" buffer hook to avoid polluting the
     ;; save-hook for non-go files.
     (when my-gofmt-installed-p
@@ -9236,8 +9237,8 @@ And turns off `indent-tabs-mode'."
 
 (with-eval-after-load 'go-mode
   ;; key binds
-  (define-key go-mode-map (kbd "C-c C-c") #'compile)
-  (define-key go-mode-map (kbd "C-c c") #'compile)
+  (define-key go-mode-map (kbd "C-c C-c") #'my-compile-at-root-dir)
+  (define-key go-mode-map (kbd "C-c c") #'my-compile-at-root-dir)
 
   ;; unbind `godef-describe' so I can use "C-c C-d" as a prefix.
   (define-key go-mode-map (kbd "C-c C-d") nil)
@@ -9250,10 +9251,11 @@ And turns off `indent-tabs-mode'."
   (defvar my-gofmt-installed-p (executable-find "gofmt"))
 
   (defun my-setup-go-mode ()
-    (when buffer-file-name ;; if buffer has a file on disk.
-      ;; wireup M-x compile. TODO: revisit this
-      (set (make-local-variable 'compile-command)
-           (concat "go run " (shell-quote-argument buffer-file-name))))
+    ;; (when buffer-file-name ;; if buffer has a file on disk.
+    ;;   ;; wireup M-x compile. TODO: revisit this
+    ;;   (set (make-local-variable 'compile-command)
+    ;;        (concat "go run " (shell-quote-argument buffer-file-name))))
+
     ;; Run gofmt on save. Use "local" buffer hook to avoid polluting the
     ;; save-hook for non-go files.
     (when my-gofmt-installed-p

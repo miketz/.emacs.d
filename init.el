2341b2a75265e04eb6470025e36760b8262861d0
mike <miketz@users.noreply.github.com>
2023-08-30 07:27:53 -0400

    helper fn. convert region text to ascii codes.

--- init.el
+++ init.el
@@ -9391,6 +9391,18 @@ And turns off `indent-tabs-mode'."
 ;;;----------------------------------------------------------------------------
 ;;; MISC options.
 ;;;----------------------------------------------------------------------------
+
+;; code sample from:
+;; https://old.reddit.com/r/emacs/comments/1v0jl1/convert_string_to_ascii_code_sequence/
+(defun charcode-region (start end)
+  "Convert the characters between START and END in current buffer
+to their character codes.  Move the unconverted text to the kill
+ring."
+  (interactive "r")
+  (let ((characters (string-to-list (buffer-substring start end))))
+    (kill-region start end)
+    (insert (mapconcat 'number-to-string characters " "))))
+
 ;; blink effect on current line when switching windows or buffers.
 (when nil
   (defun my-pulse-line-on-window-selection-change (frame)

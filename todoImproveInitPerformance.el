e981d41a7049eeaf6748d79813875728f9a151f8
mike <miketz@users.noreply.github.com>
2023-09-08 11:44:58 -0400

    track multiple startup times

--- todoImproveInitPerformance.el
+++ todoImproveInitPerformance.el
@@ -7,6 +7,6 @@ cider-style-overlays ; 3%
 my-occur-wild-spaces ; 3%
 show time in mode line (display-time-mode 1) ; 4%
 GC at end ; 34%
-  total time: 0.23s
+  total time: 0.23s, 0.18s
   (+ 11 22 3 3 4 34) ;; => 77
   unaccounted for (other stuff): ; (- 100 77) => 23%

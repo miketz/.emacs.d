e888491f8d15d933232f9147d744c6f7fc29f77f
mike <miketz@users.noreply.github.com>
2023-09-08 11:01:22 -0400

    todo file for init performance improvement

---
+++ todoImproveInitPerformance.el
@@ -0,0 +1,12 @@
+;;; with my-load-common turned off, -nw
+;;; emacs -nw -Q -l ~/.emacs.d/notElpa/profile-dotemacs.el -f profile-dotemacs
+
+key-chord ; 11%
+evil ; 22%
+cider-style-overlays ; 3%
+my-occur-wild-spaces ; 3%
+show time in mode line (display-time-mode 1) ; 4%
+GC at end ; 34%
+  total time: 0.23s
+  (+ 11 22 3 3 4 34) ;; => 77
+  unaccounted for (other stuff): ; (- 100 77) => 23%

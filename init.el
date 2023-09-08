7fa6544eb05dc6ce9f0d762e3b279f07ca462b84
mike <miketz@users.noreply.github.com>
2023-09-08 14:41:45 -0400

    move helper fns for byte compiling into separate file

--- init.el
+++ init.el
@@ -2001,35 +2001,8 @@ This prevents overlapping themes; something I would rarely want."
 ;;;----------------------------------------------------------------------------
 ;;; Recursively byte-compile every .el file
 ;;;----------------------------------------------------------------------------
-;;(byte-recompile-directory (expand-file-name "~/.emacs.d") 0)
-
-
-(defun my-delete-elc-files (dir-str)
-  "Delete all elc files in folder DIR-STR."
-  (let ((elc-files (directory-files-recursively
-                    dir-str
-                    "\.elc$")))
-    ;; Optionally, on linux manually delete. First verify file list with:
-    ;;   find . -name "*.elc" -type f
-    ;; then run:
-    ;;   find . -name "*.elc" -type f -delete
-    (cl-loop for f in elc-files
-             do
-             (delete-file f))))
-
-(defun my-byte-compile-curr-dir ()
-  "Byte compile all elisp files in the current directory."
-  (interactive)
-  ;; delete all "elc" files first. Sometimes there are issues where an
-  ;; elc is loaded which breaks compliation of an el file. Rare, but makes an
-  ;; impossible to hunt down issue when it happens.
-  (my-delete-elc-files default-directory) ; current directory
-
-  (byte-recompile-directory
-   default-directory   ; current directory
-   0                   ; 0 means compile .el files if .elc is missing.
-   t) ; t means force re-compile even if the .elc is up-to-date. May be
-  )
+(autoload #'my-delete-elc-files "my-byte-compile-stuff" nil nil)
+(autoload #'my-byte-compile-curr-dir "my-byte-compile-stuff" nil t)
 
 
 

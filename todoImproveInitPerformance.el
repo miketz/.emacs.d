2b0e39c6723356269709c8219d495553388b3c5d
mike <miketz@users.noreply.github.com>
2023-09-09 19:55:08 -0400

    comment about the 34% time spent in GC is actually a boost.

--- todoImproveInitPerformance.el
+++ todoImproveInitPerformance.el
@@ -6,7 +6,7 @@ evil ; 22%
 cider-style-overlays ; 3%
 ;; my-occur-wild-spaces ; 3%
 show time in mode line (display-time-mode 1) ; 4%
-GC at end ; 34%
+GC at end ; 34%, NOTE: this is actually performance boost. The price for GC needs to occur somewhere and we do it at the end which is faster than doing it as we go.
   total time: 0.23s, 0.18s
   (+ 11 22 3 3 4 34) ;; => 77
   unaccounted for (other stuff): ; (- 100 77) => 23%

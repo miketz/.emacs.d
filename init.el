177e92561f92c3a52f0fb3263171aa3ce4ddc84a
mike <miketz@users.noreply.github.com>
2023-12-28 12:08:01 -0500

    autoload for my-list-modules-with-upstream-code-to-merge-golang

--- init.el
+++ init.el
@@ -1338,6 +1338,8 @@ In master branch now. Was on git branch: feature/native-comp.")
 (autoload #'my-fetch-all-upstream-remotes "my-modules" nil t)
 (autoload #'my-fetch-all-upstream-remotes-golang "my-modules" nil t)
 (autoload #'my-list-modules-with-upstream-code-to-merge "my-modules" nil t)
+(autoload #'my-list-modules-with-upstream-code-to-merge-golang "my-modules"
+  nil t)
 
 
 ;;;----------------------------------------------------------------------------

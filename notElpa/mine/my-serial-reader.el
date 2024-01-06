70fdc55f70211c4d61b89570490679c05cd8d8ef
mike <miketz@users.noreply.github.com>
2024-01-05 20:39:34 -0500

    ability to cancel the timers. stopping the run.

--- notElpa/mine/my-serial-reader.el
+++ notElpa/mine/my-serial-reader.el
@@ -9,6 +9,11 @@
 
 (defvar my-delay-seconds 0.4)
 
+(defvar my-timers '()
+  "All timers we create will be stored in this list.
+This makes it easier to cancel them and clean things up.")
+
+
 (defun my-serial-reader ()
   "Entry point function.
 Display text in current buffer 1 word at a time."
@@ -19,15 +24,32 @@ Display text in current buffer 1 word at a time."
 
     (switch-to-buffer-other-window (get-buffer-create my-buff-name))
 
+    ;; stop any currently running serial reader.
+    (my-stop-serial-reader)
+
     (cl-loop for w in words
              do
              (let* ((word w)
                     (fn (lambda ()
                           (with-current-buffer (get-buffer-create my-buff-name)
                             (erase-buffer)
-                            (insert word)))))
-               (run-with-timer next-fn-delay 0 fn))
+                            (insert word))))
+                    (timer (run-with-timer next-fn-delay 0 fn)))
+               ;; keep track of the timer in my-timers so we can cancel it
+               ;; later if needed.
+               (push timer my-timers))
              (cl-incf next-fn-delay my-delay-seconds))))
+
+
+(defun my-stop-serial-reader ()
+  "Stop the display of text into buffer `my-buff-name'.
+Cancels all the timers.
+Call this if the serial display is taking too long."
+  (interactive)
+  (cl-loop for timer in my-timers
+           do
+           (cancel-timer timer))
+  (setq my-timers '()))
 
 
 (provide 'my-serial-reader)

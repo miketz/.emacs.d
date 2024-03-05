2ac4f6d77dd6ea408e6b4560227b3436fe587301
mike <miketz@users.noreply.github.com>
2024-03-05 08:47:33 -0500

    autoload for my-color-app-monochrome-themes-dark-theme

--- init.el
+++ init.el
@@ -1889,7 +1889,8 @@ This prevents overlapping themes; something I would rarely want."
   (autoload #'my-color-moe-dark file nil t)
   (autoload #'my-color-wildcharm file nil t)
   (autoload #'my-color-wildcharm-light file nil t)
-  (autoload #'my-color-ample-flat file nil t))
+  (autoload #'my-color-ample-flat file nil t)
+  (autoload #'my-color-app-monochrome-themes-dark-theme file nil t))
 
 
 (when my-graphic-p ;; transparency stuff

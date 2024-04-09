c83f16256e58e0d0a8b5f5d0b65d14b3f965452a
mike <miketz@users.noreply.github.com>
2024-04-09 11:07:15 -0400

    comment fix

--- init.el
+++ init.el
@@ -4570,7 +4570,7 @@ and indent."
 
   (defun my-avy-isearch ()
     (interactive)
-    ;; use the more limited by easier to type keys for isearch jumps
+    ;; use more limited but easier to type keys for isearch jumps
     ;; becuase isearch jumps tend to have fewer matches so less likely to need 2 keys.
     (let ((avy-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l
                          ?w ?e ?i ?o

7ed39d75501d33d001d8969b4378d0433116e058
mike <miketz@users.noreply.github.com>
2024-11-21 09:07:01 -0500

    new fn fugitive-log-between. and helper funcs to get lists of stuff.

--- notElpa/mine/fugitive.el
+++ notElpa/mine/fugitive.el
@@ -94,14 +94,56 @@ Flawed implementation:
   (fugitive-shell-command "{ git branch -vv | grep -v origin & git branch -vv | grep \": gone]\"; }"
                           (get-buffer-create fugitive-buff-name)))
 
+;;;###autoload
+(cl-defun fugitive-log-between (&optional rev1 rev2)
+  (interactive)
+  ;; get rev1, rev2 from user if needed
+  (when (or (null rev1) (null rev2))
+    (let ((revs (fugitive-get-branches-and-tags)))
+      (push "HEAD" revs) ;; TODO
+      (when (null rev1)
+        (setq rev1 (completing-read "rev1: " revs nil nil)))
+      (when (null rev2)
+        (setq rev2 (completing-read "rev2: " revs nil nil)))))
+  ;; GUARD: return early if user failed to supply rev1 or rev2
+  (when (or (null rev1) (null rev2)
+            (string-equal rev1 "")
+            (string-equal rev2 ""))
+    (message "rev1 and rev2 are required.")
+    (cl-return-from fugitive-log-between))
+  ;; run command
+  (fugitive-shell-command (format "git log %s..%s" rev1 rev2)))
+
+
+(defun fugitive-cmd-to-list (cmd)
+  "Run a git command which returns a string list as output.
+Convert the string-list to an elisp list."
+  (let* ((str (shell-command-to-string cmd))
+         (trimmed (string-trim-right str))
+         (lst (string-split trimmed "\n")))
+    lst))
 
 (defun fugitive-get-remote-aliases ()
   "Return a list of remote alias names."
-  (let* ((output (string-trim-right
-                  (shell-command-to-string "git remote")))
-         (remotes (string-split output "\n")))
-    remotes))
+  (fugitive-cmd-to-list "git remote"))
 
+(defun fugitive-get-branches-and-tags ()
+  "Return a list of branches."
+  (fugitive-cmd-to-list "git for-each-ref --format='%(refname:short)' refs/"))
+
+(defun fugitive-get-branches ()
+  "Return a list of branches."
+  (fugitive-cmd-to-list "git for-each-ref --format='%(refname:short)' refs/heads/ refs/remotes/"))
+
+(defun fugitive-get-tags ()
+  "Return a list of tags."
+  (fugitive-cmd-to-list "git for-each-ref --format='%(refname:short)' refs/tags/"))
+
+
+
+;; ;; test
+;; (let* ((default-directory "~/.emacs.d/notElpaYolo/magit"))
+;;   (fugitive-get-branches))
 
 ;; (defun fugitive-log ()
 ;;   "View git log output in an Emacs buffer."

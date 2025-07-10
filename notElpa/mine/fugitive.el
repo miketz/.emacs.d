ba5347f28148428a97e67a51769192cb382bad00
mike <miketz@users.noreply.github.com>
2025-07-10 13:32:05 -0400

    remove extra space from log cmd

--- notElpa/mine/fugitive.el
+++ notElpa/mine/fugitive.el
@@ -504,7 +504,7 @@ This is the fastest log."
   "Prepare the git command with for a log of a single file.
 Uses the file of the current buffer."
   (interactive)
-  (fugitive-shell-command (concat "git log --oneline  --pretty=format:\"%C(auto)%h %ad %C(cyan)%an%C(auto)%d %s\" " fugitive-date-format " -n 1000 -- " (fugitive-curr-filename))
+  (fugitive-shell-command (concat "git log --oneline --pretty=format:\"%C(auto)%h %ad %C(cyan)%an%C(auto)%d %s\" " fugitive-date-format " -n 1000 -- " (fugitive-curr-filename))
                           ;; (concat "git log --oneline --decorate=short -n 1000 -- " (fugitive-curr-filename))
                           nil t))
 

e1ef1153dabb57f15af9775300c944ecbda37578
mike <miketz@users.noreply.github.com>
2025-07-31 15:48:36 -0400

    let user edit regex. search for dot before funcall in find method refs

--- notElpa/mine/my-csharp-helpers.el
+++ notElpa/mine/my-csharp-helpers.el
@@ -10,11 +10,8 @@
   ;; shadow `rg-command-line-flags' for duration this let statement.
   (let* ((rg-command-line-flags rg-command-line-flags)
          (cursor-txt (thing-at-point 'symbol 'no-properties))
-         (class (completing-read "class/struct: " '() nil nil
-                                  ;; default to text under cursor
-                                  cursor-txt))
          ;; acutally a single \. double \\ is for the elisp string escape.
-         (regex (concat "(class|struct) " class)))
+         (regex (read-string "regex: " (concat "(class|struct) " cursor-txt))))
     ;; TODO: look into extra ignore options.
     ;; (add-to-list 'rg-command-line-flags "--glob '!*_test.go'")
     ;; run search
@@ -29,11 +26,8 @@
   ;; shadow `rg-command-line-flags' for duration this let statement.
   (let* ((rg-command-line-flags rg-command-line-flags)
          (cursor-txt (thing-at-point 'symbol 'no-properties))
-         (interface (completing-read "interface: " '() nil nil
-                                  ;; default to text under cursor
-                                  cursor-txt))
          ;; acutally a single \. double \\ is for the elisp string escape.
-         (regex (concat "class.+:.+" interface)))
+         (regex (read-string "regex: " (concat "class.+:.+" cursor-txt))))
     ;; TODO: look into extra ignore options.
     ;; (add-to-list 'rg-command-line-flags "--glob '!*_test.go'")
     ;; run search
@@ -68,11 +62,8 @@
   ;; shadow `rg-command-line-flags' for duration this let statement.
   (let* ((rg-command-line-flags rg-command-line-flags)
          (cursor-txt (thing-at-point 'symbol 'no-properties))
-         (fn-name (completing-read "method: " '() nil nil
-                                  ;; default to text under cursor
-                                  cursor-txt))
          ;; acutally a single \. double \\ is for the elisp string escape.
-         (regex (concat fn-name "\\(")))
+         (regex (read-string "regex: " (concat "\\." cursor-txt "\\("))))
     ;; ignore test files. this is breaking search with rg 14.1.1. comment for now
     ;; (add-to-list 'rg-command-line-flags "--glob '!*_test.go'")
     ;; run search

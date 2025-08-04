fdc3952d4cc423684f70995f0bd76bc97b188db1
mike <miketz@users.noreply.github.com>
2025-08-04 08:52:36 -0400

    comments, use bool var, require rg

--- notElpa/mine/my-jump.el
+++ notElpa/mine/my-jump.el
@@ -1,6 +1,16 @@
 ;;; my-jump.el --- Helper funcs to search with ripgrep. -*- lexical-binding: t -*-
 
+;;; License: GPL version 3
+
+;;; Keywords: convenience
+;;; Package-Requires: ((emacs "24.1") (rg 2.3.0))
+;;; Version: 0.1.0
+;;; URL: TODO
+
+
+;;; Code:
 (require 'project)
+(require 'rg)
 
 ;; TODO: look into `my-select-folder'. maybe handle git submodule issue?
 (defun my-jump-read-search-dir ()
@@ -8,7 +18,8 @@
 Attempt to use project root as default selection.
 If no project detected current dir will be the default selection."
   (let* ((proj (project-current nil))
-         (starting-dir (if (not (null proj))
+         (in-proj? (not (null proj)))
+         (starting-dir (if in-proj?
                            (project-root proj)
                          ;; else, not in a project, use current dir
                          default-directory)))

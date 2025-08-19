c9fcaf60df41c22386336805af09db4ce0734afe
mike <miketz@users.noreply.github.com>
2025-08-19 14:29:06 -0400

    set buffer local val mode-line-format for all existing buffers

--- notElpa/mine/no-mode-line.el
+++ notElpa/mine/no-mode-line.el
@@ -25,13 +25,25 @@
   (setq-default mode-line-format nil)
   (setq window-divider-default-bottom-width 1)
   (setq window-divider-default-places 'bottom-only)
-  (window-divider-mode t))
+  (window-divider-mode t)
+  ;; seting default value of `mode-line-format' works for future buffers, but not current buffers!
+  ;; have to set it for every exisitng buffer
+  (cl-loop for b in (buffer-list)
+           do
+           (with-current-buffer b
+             (setq mode-line-format nil))))
 
 (defun no-mode-line-disable ()
   "Restore the mode line. Turn off `window-divider-mode'."
   (interactive)
   (setq-default mode-line-format no-mode-line-original-format)
-  (window-divider-mode 0))
+  (window-divider-mode 0)
+  ;; seting default value of `mode-line-format' works for future buffers, but not current buffers!
+  ;; have to set it for every exisitng buffer
+  (cl-loop for b in (buffer-list)
+           do
+           (with-current-buffer b
+             (setq mode-line-format no-mode-line-original-format))))
 
 
 (provide 'no-mode-line)

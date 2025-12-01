0d5bb94977117f6f81d1c2065aa4a8498eeb326e
mike <miketz@users.noreply.github.com>
2025-12-01 11:35:10 -0500

    add real-mono-themes

---
+++ notElpa/themes/real-mono-girl-theme.el
@@ -0,0 +1,35 @@
+;;; real-mono-girl-theme.el --- A monochrome girl variant theme -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025 Qingsong Liao
+
+;; Author: Qingsong Liao <llqingsong@qq.com>
+;; Maintainer: Qingsong Liao <llqingsong@qq.com>
+;; URL: https://github.com/NestorLiao/real-mono-themes
+;; Created: 15th Nov 2025
+;; Version: 1.0.0
+;; Keywords: faces
+;; Package-Requires: ((emacs "28.1"))
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation, either version 3 of the License,
+;; or (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;  real-mono's girl theme
+
+;;; Code:
+
+(require 'real-mono-themes)
+(real-mono-themes--define-theme girl)
+
+(provide-theme 'real-mono-girl)
+;;; real-mono-girl-theme.el ends here
---
+++ notElpa/themes/real-mono-old-theme.el
@@ -0,0 +1,35 @@
+;;; real-mono-old-theme.el --- A monochrome old variant theme -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025 Qingsong Liao
+
+;; Author: Qingsong Liao <llqingsong@qq.com>
+;; Maintainer: Qingsong Liao <llqingsong@qq.com>
+;; URL: https://github.com/NestorLiao/real-mono-themes
+;; Created: 15th Nov 2025
+;; Version: 1.0.0
+;; Keywords: faces
+;; Package-Requires: ((emacs "28.1"))
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation, either version 3 of the License,
+;; or (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;  real-mono's old theme
+
+;;; Code:
+
+(require 'real-mono-themes)
+(real-mono-themes--define-theme old)
+
+(provide-theme 'real-mono-old)
+;;; real-mono-old-theme.el ends here

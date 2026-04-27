158c1ba0e663a81e67fffae6bd2de8c12d13d6d4
mike <miketz@users.noreply.github.com>
2026-04-27 09:50:42 -0400

    set base tab-line face

--- notElpa/themes/charcoal-theme.el
+++ notElpa/themes/charcoal-theme.el
@@ -739,6 +739,8 @@ Example:
 
 
    ;; tab-line.  like web browser tabs.
+   `(tab-line ((,class ;:inherit variable-pitch
+                       :background "#103535")))
    `(tab-line-tab ((,class :inherit tab-line :foreground "white" :background "#705050")))
    `(tab-line-tab-current ((,class :inherit tab-line-tab :background "#301010")))
    `(tab-line-tab-inactive ((,class :inherit tab-line-tab)))

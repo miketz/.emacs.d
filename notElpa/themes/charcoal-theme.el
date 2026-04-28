09c2aea555bb3b2ad28cfeba278602f6c0270dcc
mike <miketz@users.noreply.github.com>
2026-04-28 14:18:15 -0400

    tab-line-tab-special face. remove bold

--- notElpa/themes/charcoal-theme.el
+++ notElpa/themes/charcoal-theme.el
@@ -749,7 +749,7 @@ Example:
    `(tab-line-tab-modified ((,class :foreground "red" )))
    `(tab-line-highlight ((,class :foreground "yellowgreen")))
    ;; tab-line-close-highlight
-   ;; tab-line-tab-special
+   `(tab-line-tab-special ((,class :foreground "#BDBDFD" :slant italic)))
    ;; tab-line-tab-group
 
 
